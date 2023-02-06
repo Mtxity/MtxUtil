3c7c27b641fe6cc0679c7e54fcef45f4934ec5ac
Enrique <edavalos@ucsc.edu>
2023-02-05 21:27:02 -0800

    Edited time validation regex in MtxTimeUtil

--- src/main/java/com/edavalos/mtx/util/time/MtxTimeUtil.java
+++ src/main/java/com/edavalos/mtx/util/time/MtxTimeUtil.java
@@ -9,7 +9,7 @@ public final class MtxTimeUtil {
         HOURS
     }
 
-    private static final Pattern REGEX_VALID_TIME = Pattern.compile("^(([0-1])?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$");
+    private static final Pattern REGEX_VALID_TIME = Pattern.compile("^(([0-1])?[0-9]|2[0-3]):([0-5])?[0-9](:([0-5])?[0-9])?$");
 
     private MtxTimeUtil() { }
 
