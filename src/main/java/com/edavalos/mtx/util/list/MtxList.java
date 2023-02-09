b987409ea5577868ce8b79151a7d867ceb47a108
Enrique <edavalos@ucsc.edu>
2023-02-09 00:23:02 -0800

    Added notes of which MtxList methods have been implemented

--- src/main/java/com/edavalos/mtx/util/list/MtxList.java
+++ src/main/java/com/edavalos/mtx/util/list/MtxList.java
@@ -10,29 +10,29 @@ public interface MtxList<T> {
     /**
      * @return size/length of this list
      */
-    int size();
+    int size(); // DONE
 
     /**
      * @return true if this list is empty, false otherwise
      */
-    boolean isEmpty();
+    boolean isEmpty(); // DONE
 
     /**
      * @return true if this list contains the specified element, false otherwise
      */
-    boolean contains(T element);
+    boolean contains(T element); // DONE
 
     /**
      * @return true if this list contains all the specified elements, false otherwise
      */
     // TODO: Add unit tests
-    boolean containsAll(Collection<T> elements);
-    boolean containsAll(T[] elements);
+    boolean containsAll(Collection<T> elements); // DONE
+    boolean containsAll(T[] elements); // DONE
 
     /**
      * @return an iterator of this list
      */
-    Iterator<T> iterator();
+    Iterator<T> iterator(); // DONE
 
     /**
      * @return an array of this list
@@ -49,20 +49,20 @@ public interface MtxList<T> {
     /**
      * Adds an element to this list
      */
-    void add(T element);
+    void add(T element); // DONE
 
     /**
      * Adds all elements to this list
      */
     // TODO: Add unit tests
-    void addAll(Collection<T> elements);
-    void addAll(T[] elements);
+    void addAll(Collection<T> elements); // DONE
+    void addAll(T[] elements); // DONE
 
     /**
      * Removes an element from this list
      * @return true if element was found (and removed), false if element was not found
      */
-    boolean remove(T element);
+    boolean remove(T element); // DONE
 
     /**
      * Removes all elements from this list
@@ -71,8 +71,8 @@ public interface MtxList<T> {
      * came before it in the given list will still have been removed.
      */
     // TODO: Add unit tests
-    boolean removeAll(Collection<T> elements);
-    boolean removeAll(T[] elements);
+    boolean removeAll(Collection<T> elements); // DONE
+    boolean removeAll(T[] elements); // DONE
 
     /**
      * Sorts this list
@@ -82,25 +82,25 @@ public interface MtxList<T> {
     /**
      * Removes everything from this list
      */
-    void clear();
+    void clear(); // DONE
 
     /**
      * @return true if all elements in this list satisfy .equals() with given object, false otherwise
      */
     @Override
-    boolean equals(Object o);
+    boolean equals(Object o); // DONE
 
     /**
      * @return the hashcode of this list
      */
     @Override
-    int hashCode();
+    int hashCode(); // DONE
 
     /**
      * @return a string representation of this list
      */
     @Override
-    String toString();
+    String toString(); // DONE
 
     // --------------- Positional Access Operations ----------------
 
@@ -108,7 +108,7 @@ public interface MtxList<T> {
      * @return the element at the given index
      * @throws IndexOutOfBoundsException when the index is larger than the size of this list minus one (or is negative)
      */
-    T get(int index) throws IndexOutOfBoundsException;
+    T get(int index) throws IndexOutOfBoundsException; // DONE
 
     /**
      * Sets the element at the given index to the given element
@@ -127,12 +127,12 @@ public interface MtxList<T> {
     /**
      * @return the index of the given element, or -1 if the element could not be found
      */
-    int indexOf(T element);
+    int indexOf(T element); // DONE
 
     /**
      * @return the number of occurrences of the given element in this list
      */
-    int countOccurrences(T element);
+    int countOccurrences(T element); // DONE
 
     /**
      * @return a new list starting at the given fromIndex and ending at the given toIndex
