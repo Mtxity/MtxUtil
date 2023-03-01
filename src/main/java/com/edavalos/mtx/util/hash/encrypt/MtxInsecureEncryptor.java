e0bff9102956426a12ede7d84a12f1b13b44c9c4
Enrique <edavalos@ucsc.edu>
2023-02-28 22:56:25 -0800

    Fixed MtxInsecureEncryptor

--- src/main/java/com/edavalos/mtx/util/hash/encrypt/MtxInsecureEncryptor.java
+++ src/main/java/com/edavalos/mtx/util/hash/encrypt/MtxInsecureEncryptor.java
@@ -1,38 +1,32 @@
 package com.edavalos.mtx.util.hash.encrypt;
 
 public final class MtxInsecureEncryptor {
+    private static final char WHITESPACE = ' ';
 
     private MtxInsecureEncryptor() { }
 
     public static String encrypt(String string) {
         char[] deconstructedString = string.toCharArray();
         int l = deconstructedString.length;
-        int a = (int) Math.floor(Math.sqrt(l));
-        int b = (int) Math.ceil(Math.sqrt(l));
-        if (a * b < l) {
-            if (Math.min(b, a) == b) {
-                b++;
-            } else {
-                a++;
-            }
-        }
+        int x = (int) Math.ceil(Math.sqrt(l));
 
         // Generate encryption matrix
-        char[][] matrix = new char[a][b];
+        char[][] matrix = new char[x][x];
         int k = 0;
-        for (int row = 0; row < a; row++) {
-            for (int col = 0; col < b; col++) {
+        for (int row = 0; row < x; row++) {
+            for (int col = 0; col < x; col++) {
                 if (k < l) {
-                    matrix[row][col] = deconstructedString[k];
+                    matrix[row][col] = deconstructedString[k++];
+                } else {
+                    matrix[row][col] = WHITESPACE;
                 }
-                k++;
             }
         }
 
         // Generate encrypted string
         StringBuilder encryptedString = new StringBuilder();
-        for (int row2 = 0; row2 < b; row2++) {
-            for (int col2 = 0; col2 < a; col2++) {
+        for (int row2 = 0; row2 < x; row2++) {
+            for (int col2 = 0; col2 < x; col2++) {
                 // rows and columns are flipped
                 encryptedString.append(matrix[col2][row2]);
             }
@@ -44,41 +38,30 @@ public final class MtxInsecureEncryptor {
     public static String decrypt(String encryptedString) {
         char[] deconstructedEncryptedString = encryptedString.toCharArray();
         int l = deconstructedEncryptedString.length;
-        int a = (int) Math.floor(Math.sqrt(l));
-        int b = (int) Math.ceil(Math.sqrt(l));
-        while (a != b) {
-            if (Math.min(b, a) == b) {
-                b++;
-            } else {
-                a++;
-            }
-        }
+        int y = (int) Math.ceil(Math.sqrt(l));
 
         // Generate decryption matrix
-        char[][] matrix = new char[a][b];
+        char[][] matrix = new char[y][y];
         int k = 0;
-        for (int row = 0; row < a; row++) {
-            for (int col = 0; col < b; col++) {
+        for (int row = 0; row < y; row++) {
+            for (int col = 0; col < y; col++) {
                 if (k < l) {
-                    matrix[row][col] = deconstructedEncryptedString[k];
+                    matrix[row][col] = deconstructedEncryptedString[k++];
+                } else {
+                    matrix[row][col] = WHITESPACE;
                 }
-                k++;
             }
         }
 
         // Decrypt string
         StringBuilder decryptedString = new StringBuilder();
-        for (int row2 = 0; row2 < a; row2++) {
-            for (int col2 = 0; col2 < b; col2++) {
-                // null chars may appear
-                if (matrix[col2][row2] == '\u0000') {
-                    continue;
-                }
+        for (int row2 = 0; row2 < y; row2++) {
+            for (int col2 = 0; col2 < y; col2++) {
                 // rows and columns are flipped
                 decryptedString.append(matrix[col2][row2]);
             }
         }
 
-        return decryptedString.toString().strip();
+        return decryptedString.toString().stripTrailing();
     }
 }
