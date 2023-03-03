a8d7daab15a504f1740224e11f84707ede91a286
Enrique <edavalos@ucsc.edu>
2023-03-03 00:58:30 -0800

    Added removeVertex() to MtxAdjacencyListGraoh

--- src/main/java/com/edavalos/mtx/util/graph/MtxAdjacencyListGraph.java
+++ src/main/java/com/edavalos/mtx/util/graph/MtxAdjacencyListGraph.java
@@ -11,4 +11,10 @@ public final class MtxAdjacencyListGraph {
     public void addVertex(String label) {
         adjacencyVertices.putIfAbsent(new MtxVertex(label), new ArrayList<>());
     }
+
+    public void removeVertex(String label) {
+        MtxVertex v = new MtxVertex(label);
+        adjacencyVertices.values().stream().forEach(e -> e.remove(v));
+        adjacencyVertices.remove(new MtxVertex(label));
+    }
 }
