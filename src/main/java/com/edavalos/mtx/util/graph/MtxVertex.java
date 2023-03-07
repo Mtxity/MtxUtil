764a0b3d95b86ecc23faa7fea8781e4b4febddb0
Enrique <edavalos@ucsc.edu>
2023-03-06 19:11:36 -0800

    Added equals() and hashCode() methods to MtxVertex

--- src/main/java/com/edavalos/mtx/util/graph/MtxVertex.java
+++ src/main/java/com/edavalos/mtx/util/graph/MtxVertex.java
@@ -1,3 +1,19 @@
 package com.edavalos.mtx.util.graph;
 
-public record MtxVertex(String label) { }
+import java.util.Objects;
+
+public record MtxVertex(String label) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof MtxVertex v) {
+            return this.label.equals(v.label);
+        }
+        return o.equals(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
+    }
+}
