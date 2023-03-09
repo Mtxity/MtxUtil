8d0b554e40656ff28df6f12cf090a1199355e782
Enrique <edavalos@ucsc.edu>
2023-03-08 21:39:31 -0800

    Added MtxMatrixFormatter (WIP)
    Added format() method

---
+++ src/main/java/com/edavalos/mtx/util/string/MtxMatrixFormatter.java
@@ -0,0 +1,32 @@
+package com.edavalos.mtx.util.string;
+
+public final class MtxMatrixFormatter {
+    private MtxMatrixFormatter() { }
+
+    public static String format(String[][] matrix) {
+        int maxLen = findMaxLengthInMatrix(matrix) + 1;
+
+        String s = "";
+        for (String[] row : matrix) {
+            for (String col : row) {
+                s += padString(col, maxLen);
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
+    private static int findMaxLengthInMatrix(String[][] mat) {
+        int len = 0;
+        for (String[] row : mat) {
+            for (String col : row) {
+                len = Math.max(len, col.length());
+            }
+        }
+        return len;
+    }
+
+    private static String padString(String s, int length) {
+        return new String(new char[length - s.length()]).replace('\0', ' ') + s;
+    }
+}
