06b3ad019e3b27dff0bb7e9ddd9c30f27c4eae94
Enrique <edavalos@ucsc.edu>
2023-03-15 00:33:56 -0700

    Added splitCharsAt() to MtxStringUtil

--- src/main/java/com/edavalos/mtx/util/string/MtxStringUtil.java
+++ src/main/java/com/edavalos/mtx/util/string/MtxStringUtil.java
@@ -1,9 +1,28 @@
 package com.edavalos.mtx.util.string;
 
+import java.util.regex.Pattern;
+
 public final class MtxStringUtil {
     private MtxStringUtil() { }
 
     public static boolean isEmpty(String string) {
         return string == null || "".equals(string);
+    }
+
+    public static String[] splitAtChars(String string, char... chars) {
+        if (chars.length == 0) {
+            return new String[]{string};
+        }
+
+        if (chars.length == 1) {
+            return string.split(Pattern.quote(String.valueOf(chars[0])));
+        }
+
+        StringBuilder spliterator = new StringBuilder();
+        for (char c : chars) {
+            spliterator.append(c).append("|");
+        }
+        spliterator.deleteCharAt(spliterator.length() - 1);
+        return string.split(spliterator.toString());
     }
 }
