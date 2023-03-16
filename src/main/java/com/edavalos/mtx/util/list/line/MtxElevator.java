95764863c8b344240bf3c836eb25e8ee5a1e3918
Enrique <edavalos@ucsc.edu>
2023-03-15 20:38:36 -0700

    Added MtxElevator skeleton

---
+++ src/main/java/com/edavalos/mtx/util/list/line/MtxElevator.java
@@ -0,0 +1,46 @@
+package com.edavalos.mtx.util.list.line;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public final class MtxElevator<C> {
+    @FunctionalInterface
+    public interface MtxElevatorContentsManager<T> {
+        T modifyContents(T contents);
+    }
+
+    private final int floors;
+    private final SortedMap<Integer, Integer> floorQueue;
+    private int direction;
+    private int currentFloor;
+    private C contents;
+
+    public MtxElevator(int floors) {
+        if (floors < 0) {
+            throw new IndexOutOfBoundsException("Negative numbers are not allowed: " + floors);
+        }
+
+        this.floors = floors;
+        this.floorQueue = new TreeMap<>();
+        this.direction = 0;
+        this.currentFloor = 0;
+        this.contents = null;
+    }
+
+    public void queueFloor(int startingFloor, int destinationFloor) {
+
+    }
+
+    public void moveToNextStop() {
+
+    }
+
+    public C interactWithContents(MtxElevatorContentsManager<C> contentsManager) {
+        return contentsManager.modifyContents(this.contents);
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
