cc097fa374e75201dd35ac77f43a6dd640bde3a5
Enrique <edavalos@ucsc.edu>
2023-03-16 21:28:12 -0700

    Added WIP solutions to queueFloor() and moveToNextStop() in MtxElevator

--- src/main/java/com/edavalos/mtx/util/list/line/MtxElevator.java
+++ src/main/java/com/edavalos/mtx/util/list/line/MtxElevator.java
@@ -1,7 +1,8 @@
 package com.edavalos.mtx.util.list.line;
 
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public final class MtxElevator<C> {
     @FunctionalInterface
@@ -9,8 +10,20 @@ public final class MtxElevator<C> {
         T modifyContents(T contents);
     }
 
+    // Direction key:
+    //  1 : going up
+    //  0 : standing still
+    // -1 : going down
+
+    // Typical order of operations for an elevator:
+    // 1. queue floors (can happen whenever)
+    // 2. move to next stop
+    // 3. interact with contents
+    // 4. go to step 2.
+
     private final int floors;
-    private final SortedMap<Integer, Integer> floorQueue;
+    private final List<Integer> upwardsFloorQueue;
+    private final List<Integer> downwardsFloorQueue;
     private int direction;
     private int currentFloor;
     private C contents;
@@ -21,18 +34,86 @@ public final class MtxElevator<C> {
         }
 
         this.floors = floors;
-        this.floorQueue = new TreeMap<>();
+        this.upwardsFloorQueue = new ArrayList<>();
+        this.downwardsFloorQueue = new ArrayList<>();
         this.direction = 0;
         this.currentFloor = 0;
         this.contents = null;
     }
 
     public void queueFloor(int startingFloor, int destinationFloor) {
-
+        if (startingFloor == destinationFloor) {
+            // Already at destination
+            return;
+        }
+        if (startingFloor > destinationFloor) {
+            this.upwardsFloorQueue.add(destinationFloor);
+        }
+        if (startingFloor < destinationFloor) {
+            this.downwardsFloorQueue.add(destinationFloor);
+        }
     }
 
     public void moveToNextStop() {
+        // Already there
+        if (this.upwardsFloorQueue.contains(this.currentFloor)) {
+            this.upwardsFloorQueue.remove(this.currentFloor);
+        }
+        if (this.downwardsFloorQueue.contains(this.currentFloor)) {
+            this.downwardsFloorQueue.remove(this.currentFloor);
+        }
 
+        // Going up
+        if (this.direction > 0) {
+            Collections.sort(this.upwardsFloorQueue);
+            for (int i = 0; i < this.upwardsFloorQueue.size(); i++) {
+                // If floor is lower than current, ignore
+                if (this.upwardsFloorQueue.get(i) < this.currentFloor) {
+                    continue;
+                }
+                // Move up to next floor and remove it from queue
+                this.currentFloor = this.upwardsFloorQueue.remove(i);
+                return;
+            }
+            // If no floors higher than current are found, switch direction
+            if (this.downwardsFloorQueue.isEmpty()) {
+                this.direction = 0;
+            } else {
+                this.direction = -1;
+            }
+            return;
+        }
+
+        // Going down
+        if (this.direction < 0) {
+            Collections.sort(this.downwardsFloorQueue);
+            // If all floors are higher than current, switch directions
+            if (this.downwardsFloorQueue.get(0) > this.currentFloor) {
+                this.direction = 1;
+            }
+            // Move down to next floor and remove it from queue
+            this.currentFloor = this.downwardsFloorQueue.remove(0);
+
+            // If no floors lower than current are found, switch direction
+            if (this.downwardsFloorQueue.isEmpty()) {
+                if (this.upwardsFloorQueue.isEmpty()) {
+                    this.direction = 0;
+                } else {
+                    this.direction = 1;
+                    this.currentFloor = this.upwardsFloorQueue.get(0);
+                }
+            }
+            return;
+        }
+
+        // Standing still
+        if (!this.upwardsFloorQueue.isEmpty()) {
+            this.direction = 1;
+            this.moveToNextStop();
+        } else if (!this.downwardsFloorQueue.isEmpty()) {
+            this.direction = -1;
+            this.moveToNextStop();
+        }
     }
 
     public C interactWithContents(MtxElevatorContentsManager<C> contentsManager) {
@@ -40,7 +121,7 @@ public final class MtxElevator<C> {
     }
 
     public int queueSize() {
-        return this.floorQueue.size();
+        return this.upwardsFloorQueue.size() + this.downwardsFloorQueue.size();
     }
 
     public int getCurrentFloor() {
