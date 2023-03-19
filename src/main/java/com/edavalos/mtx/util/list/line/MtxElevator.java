08d44d6aca7249b15757adcc823e1239453cbb15
Enrique <edavalos@ucsc.edu>
2023-03-19 01:50:53 -0700

    Updated MtxElevator

--- src/main/java/com/edavalos/mtx/util/list/line/MtxElevator.java
+++ src/main/java/com/edavalos/mtx/util/list/line/MtxElevator.java
@@ -24,6 +24,7 @@ public final class MtxElevator<C> {
     private final int floors;
     private final List<Integer> upwardsFloorQueue;
     private final List<Integer> downwardsFloorQueue;
+    // @TODO: Make this an enum
     private int direction;
     private int currentFloor;
     private C contents;
@@ -51,68 +52,105 @@ public final class MtxElevator<C> {
             // Already at destination
             return;
         }
+
+        int newDirection = 0;
         if (startingFloor < destinationFloor) {
             this.upwardsFloorQueue.add(destinationFloor);
+            newDirection = 1;
         }
         if (startingFloor > destinationFloor) {
             this.downwardsFloorQueue.add(destinationFloor);
+            newDirection = -1;
+        }
+
+        if (this.direction == 0) {
+            this.direction = newDirection;
         }
     }
 
     public void moveToNextFloor() {
-        // Already there
-        if (this.upwardsFloorQueue.contains(this.currentFloor)) {
-            this.upwardsFloorQueue.remove(this.currentFloor);
-        }
-        if (this.downwardsFloorQueue.contains(this.currentFloor)) {
-            this.downwardsFloorQueue.remove(this.currentFloor);
-        }
-
+//        // Already there
+//        if (this.upwardsFloorQueue.contains(this.currentFloor)) {
+//            this.upwardsFloorQueue.remove(this.currentFloor);
+//        }
+//        if (this.downwardsFloorQueue.contains(this.currentFloor)) {
+//            this.downwardsFloorQueue.remove(this.currentFloor);
+//        }
+//
+//        // Going up
+//        if (this.direction > 0) {
+//            Collections.sort(this.upwardsFloorQueue);
+//            for (int i = 0; i < this.upwardsFloorQueue.size(); i++) {
+//                // If floor is lower than current, ignore
+//                if (this.upwardsFloorQueue.get(i) < this.currentFloor) {
+//                    continue;
+//                }
+//                // Move up to next floor and remove it from queue
+//                this.currentFloor = this.upwardsFloorQueue.remove(i);
+//                break;
+//            }
+//            // If no floors higher than current are found, switch direction
+//            if (this.upwardsFloorQueue.isEmpty()) {
+//                if (this.downwardsFloorQueue.isEmpty()) {
+//                    this.direction = 0;
+//                } else {
+//                    this.direction = -1;
+//                }
+//            }
+//            return;
+//        }
+//
+//        // Going down
+//        if (this.direction < 0) {
+//            Collections.sort(this.downwardsFloorQueue);
+//            // If all floors are higher than current, switch directions
+//            if (this.downwardsFloorQueue.get(0) > this.currentFloor) {
+//                this.direction = 1;
+//            }
+//            // Move down to next floor and remove it from queue
+//            this.currentFloor = this.downwardsFloorQueue.remove(this.downwardsFloorQueue.size() - 1);
+//
+//            // If no floors lower than current are found, switch direction
+//            if (this.downwardsFloorQueue.isEmpty()) {
+//                if (this.upwardsFloorQueue.isEmpty()) {
+//                    this.direction = 0;
+//                } else {
+//                    this.direction = 1;
+//                    this.currentFloor = this.upwardsFloorQueue.get(0);
+//                }
+//            }
+//            return;
+//        }
+//
+//        // Standing still
+//        if (!this.upwardsFloorQueue.isEmpty()) {
+//            this.direction = 1;
+//            this.moveToNextFloor();
+//        } else if (!this.downwardsFloorQueue.isEmpty()) {
+//            this.direction = -1;
+//            this.moveToNextFloor();
+//        }
         // Going up
-        if (this.direction > 0) {
+        if (this.direction == 1) {
             Collections.sort(this.upwardsFloorQueue);
             for (int i = 0; i < this.upwardsFloorQueue.size(); i++) {
-                // If floor is lower than current, ignore
-                if (this.upwardsFloorQueue.get(i) < this.currentFloor) {
-                    continue;
-                }
-                // Move up to next floor and remove it from queue
-                this.currentFloor = this.upwardsFloorQueue.remove(i);
-                break;
-            }
-            // If no floors higher than current are found, switch direction
-            if (this.upwardsFloorQueue.isEmpty()) {
-                if (this.downwardsFloorQueue.isEmpty()) {
-                    this.direction = 0;
-                } else {
-                    this.direction = -1;
+                if (this.upwardsFloorQueue.get(i) >= this.currentFloor) {
+                    this.currentFloor = this.upwardsFloorQueue.remove(i);
+                    return;
                 }
             }
-            return;
         }
-
         // Going down
-        if (this.direction < 0) {
+        if (this.direction == -1) {
             Collections.sort(this.downwardsFloorQueue);
-            // If all floors are higher than current, switch directions
-            if (this.downwardsFloorQueue.get(0) > this.currentFloor) {
-                this.direction = 1;
-            }
-            // Move down to next floor and remove it from queue
-            this.currentFloor = this.downwardsFloorQueue.remove(this.downwardsFloorQueue.size() - 1);
-
-            // If no floors lower than current are found, switch direction
-            if (this.downwardsFloorQueue.isEmpty()) {
-                if (this.upwardsFloorQueue.isEmpty()) {
-                    this.direction = 0;
-                } else {
-                    this.direction = 1;
-                    this.currentFloor = this.upwardsFloorQueue.get(0);
+            Collections.reverse(this.downwardsFloorQueue);
+            for (int i = this.downwardsFloorQueue.size() - 1; i <= 0; i--) {
+                if (this.downwardsFloorQueue.get(i) <= this.currentFloor) {
+                    this.currentFloor = this.downwardsFloorQueue.remove(i);
+                    return;
                 }
             }
-            return;
         }
-
         // Standing still
         if (!this.upwardsFloorQueue.isEmpty()) {
             this.direction = 1;
@@ -121,6 +159,7 @@ public final class MtxElevator<C> {
             this.direction = -1;
             this.moveToNextFloor();
         }
+        
     }
 
     public C interactWithContents(MtxElevatorContentsManager<C> contentsManager) {
