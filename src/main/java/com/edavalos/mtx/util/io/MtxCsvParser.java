587ff73db5c4a8d7baeeb029c821d2ccc11251ba
Enrique <edavalos@ucsc.edu>
2023-03-28 21:13:13 -0700

    Finished MtxCsvParser

--- src/main/java/com/edavalos/mtx/util/io/MtxCsvParser.java
+++ src/main/java/com/edavalos/mtx/util/io/MtxCsvParser.java
@@ -1,8 +1,13 @@
 package com.edavalos.mtx.util.io;
 
+import com.edavalos.mtx.util.string.MtxMatrixFormatter;
+import com.edavalos.mtx.util.string.MtxStringUtil;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -19,8 +24,25 @@ public final class MtxCsvParser {
         }
 
         String[] fileName = filePath.split(Pattern.quote("."));
-        if (!fileName[fileName.length - 1].endsWith(".csv")) {
+        if (!fileName[fileName.length - 1].equalsIgnoreCase("csv")) {
             throw new IOException("The file '" + filePath + "' is not a csv file.");
         }
+
+        List<String[]> rows = new ArrayList<>();
+        while (fileScanner.hasNextLine()) {
+            rows.add(MtxStringUtil.splitAtCommas(fileScanner.nextLine()));
+        }
+        fileScanner.close();
+
+        this.csvContents = rows.toArray(new String[0][]);
+    }
+
+    public String[][] getCsvContents() {
+        return this.csvContents;
+    }
+
+    @Override
+    public String toString() {
+        return MtxMatrixFormatter.formatBorder(this.csvContents);
     }
 }
