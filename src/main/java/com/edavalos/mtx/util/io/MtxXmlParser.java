5875b7e1e706d0118595f4907622840cd9bcd30e
Enrique <edavalos@ucsc.edu>
2023-03-31 02:20:54 -0700

    Added parseOpeningTag() to MtxXmlParser

--- src/main/java/com/edavalos/mtx/util/io/MtxXmlParser.java
+++ src/main/java/com/edavalos/mtx/util/io/MtxXmlParser.java
@@ -1,11 +1,73 @@
 package com.edavalos.mtx.util.io;
 
-import java.util.HashMap;
+import com.edavalos.mtx.util.string.MtxStringUtil;
 
-public final class MtxXmlParser {
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+public class MtxXmlParser {
+    private static final String TAG_NAME_MARKER = ".NAME";
+
     private final HashMap<String, Object> contents;
 
     public MtxXmlParser(String filePath) {
         this.contents = new HashMap<>();
+    }
+
+    protected static HashMap<String, String> parseOpeningTag(String tag) throws ParseException {
+        tag = tag.strip();
+        if (!tag.startsWith("<")) {
+            throw new ParseException(tag, 0);
+        } else if (!tag.endsWith(">")) {
+            throw new ParseException(tag, tag.length());
+        }
+
+        tag = tag.replaceFirst(Pattern.quote("<"), "").replaceAll(">$", "");
+        if (tag.contains("<")) {
+            throw new ParseException(tag, tag.indexOf('<'));
+        } else if (tag.contains(">")) {
+            throw new ParseException(tag, tag.indexOf('>'));
+        } else if (MtxStringUtil.countOccurrencesOf(tag, "/") != 1) {
+            throw new ParseException(tag, tag.indexOf('/'));
+        }
+
+        String[] tagComponents = tag.replaceAll("/$", "").split(Pattern.quote(" "));
+        if (tagComponents.length == 0) {
+            throw new ParseException(tag, 1);
+        }
+
+        HashMap<String, String> tagContentsMap = new HashMap<>();
+        int idx = 0;
+        for (int i = 0; i < tagComponents.length; i++) {
+            if (tagComponents[i].length() == 0) {
+                continue;
+            }
+
+            if (i == 0) {
+                if (tagComponents[0].contains("\"")) {
+                    throw new ParseException(tag, tag.indexOf('"'));
+                } else {
+                    tagContentsMap.put(TAG_NAME_MARKER, tagComponents[0]);
+                }
+
+            } else {
+                String[] kv = tagComponents[i].split(Pattern.quote("="));
+                if (kv.length != 2) {
+                    throw new ParseException(tag, idx);
+                } else {
+                    if (!kv[1].startsWith("\"") ||
+                        !kv[1].endsWith("\"") ||
+                        MtxStringUtil.countOccurrencesOf(kv[1], "\"") > 2) {
+                        throw new ParseException(tag, idx);
+                    }
+                }
+                tagContentsMap.put(kv[0], kv[1].replaceAll("\"", ""));
+            }
+
+            idx += tagComponents[i].length() + 1;
+        }
+
+        return tagContentsMap;
     }
 }
