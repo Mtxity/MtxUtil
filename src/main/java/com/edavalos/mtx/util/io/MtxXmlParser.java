0a95a2d62615020fce931b64453866b3dbd7915e
Enrique <edavalos@ucsc.edu>
2023-03-30 23:51:49 -0700

    Added WIP MtxXmlParser

---
+++ src/main/java/com/edavalos/mtx/util/io/MtxXmlParser.java
@@ -0,0 +1,11 @@
+package com.edavalos.mtx.util.io;
+
+import java.util.HashMap;
+
+public final class MtxXmlParser {
+    private final HashMap<String, Object> contents;
+
+    public MtxXmlParser(String filePath) {
+        this.contents = new HashMap<>();
+    }
+}
