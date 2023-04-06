7da14ede7e432b38756ed2dccdc63fffbabfe79f
Enrique <edavalos@ucsc.edu>
2023-04-05 22:24:46 -0700

    Moved parseTag() tests into own nested class

--- src/test/java/com/edavalos/mtx/util/io/MtxXmlParserTest.java
+++ src/test/java/com/edavalos/mtx/util/io/MtxXmlParserTest.java
@@ -1,6 +1,7 @@
 package com.edavalos.mtx.util.io;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 
 import java.text.ParseException;
 import java.util.HashMap;
@@ -132,62 +133,66 @@ public final class MtxXmlParserTest {
         assertThrows(ParseException.class, () -> MtxXmlParser.parseTag("<!-- test"));
     }
 
-    @Test
-    public void testSeparateParts() {
-        String sampleXml = "<n1><n2> <n3 t=\"2\"></n3><o1></o1>  </n2></n1>";
-        String[] sampleXmlDeconstructed = new String[] {
-                "<n1>",
+    @Nested
+    class SeparatePartsTests {
+
+        @Test
+        public void testSeparateParts() {
+            String sampleXml = "<n1><n2> <n3 t=\"2\"></n3><o1></o1>  </n2></n1>";
+            String[] sampleXmlDeconstructed = new String[]{
+                    "<n1>",
                     "<n2>",
-                        "<n3 t=\"2\">",
-                        "</n3>",
-                        "<o1>",
-                        "</o1>",
+                    "<n3 t=\"2\">",
+                    "</n3>",
+                    "<o1>",
+                    "</o1>",
                     "</n2>",
-                "</n1>",
-        };
+                    "</n1>",
+            };
 
-        assertArrayEquals(sampleXmlDeconstructed, MtxXmlParser.separateParts(sampleXml));
-    }
+            assertArrayEquals(sampleXmlDeconstructed, MtxXmlParser.separateParts(sampleXml));
+        }
 
-    @Test
-    public void testSeparateParts_withTextInBetween() {
-        String sampleXml = "<n1 k=\"a\"><n2><n3>inner spot a</n3><o1>inner spot b</o1></n2></n1>";
-        String[] sampleXmlDeconstructed = new String[] {
-                "<n1 k=\"a\">",
+        @Test
+        public void testSeparateParts_withTextInBetween() {
+            String sampleXml = "<n1 k=\"a\"><n2><n3>inner spot a</n3><o1>inner spot b</o1></n2></n1>";
+            String[] sampleXmlDeconstructed = new String[]{
+                    "<n1 k=\"a\">",
                     "<n2>",
-                        "<n3>inner spot a</n3>",
-                        "<o1>inner spot b</o1>",
+                    "<n3>inner spot a</n3>",
+                    "<o1>inner spot b</o1>",
                     "</n2>",
-                "</n1>",
-        };
+                    "</n1>",
+            };
 
-        assertArrayEquals(sampleXmlDeconstructed, MtxXmlParser.separateParts(sampleXml));
-    }
+            assertArrayEquals(sampleXmlDeconstructed, MtxXmlParser.separateParts(sampleXml));
+        }
 
-    @Test
-    public void testSeparateParts_withSpacesInBetween() {
-        String sampleXml =
-                """
-                <n1>
-                    <n2>
-                        <n3 t="2">
-                        </n3>
-                        <o1></o1>
-                    </n2>
-                </n1>
-                """;
-        String[] sampleXmlDeconstructed = new String[] {
-                "<n1>",
+        @Test
+        public void testSeparateParts_withSpacesInBetween() {
+            String sampleXml =
+                    """
+                            <n1>
+                                <n2>
+                                    <n3 t="2">
+                                    </n3>
+                                    <o1></o1>
+                                </n2>
+                            </n1>
+                            """;
+            String[] sampleXmlDeconstructed = new String[]{
+                    "<n1>",
                     "<n2>",
-                        "<n3 t=\"2\">",
-                        "</n3>",
-                        "<o1>",
-                        "</o1>",
+                    "<n3 t=\"2\">",
+                    "</n3>",
+                    "<o1>",
+                    "</o1>",
                     "</n2>",
-                "</n1>",
-        };
+                    "</n1>",
+            };
 
-        assertArrayEquals(sampleXmlDeconstructed, MtxXmlParser.separateParts(sampleXml));
+            assertArrayEquals(sampleXmlDeconstructed, MtxXmlParser.separateParts(sampleXml));
+        }
     }
 
     @Test
