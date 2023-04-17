f7c13631f97e77174363a045de87e1f4d8605bb1
Enrique <edavalos@ucsc.edu>
2023-04-16 23:45:01 -0700

    Added parsed json example to MtxJsonParserTest

--- src/test/java/com/edavalos/mtx/util/io/MtxJsonParserTest.java
+++ src/test/java/com/edavalos/mtx/util/io/MtxJsonParserTest.java
@@ -3,6 +3,8 @@ package com.edavalos.mtx.util.io;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -11,6 +13,48 @@ import static org.junit.jupiter.api.Assertions.fail;
 public final class MtxJsonParserTest {
     private static final String CURRENT_FILE_PATH = System.getProperty("user.dir") + "\\src\\test\\java\\com\\edavalos\\mtx\\util\\io\\";
     private static final String SAMPLE_JSON_FILENAME = "sampleJsonFile.json";
+    private static final LinkedHashMap<String, Object> SAMPLE_PARSED_JSON = new LinkedHashMap<>() {{
+        put("glossary", new LinkedHashMap<String, Object>() {{
+            put("title", "example glossary");
+            put("GlossDiv", new LinkedHashMap<String, Object>() {{
+                put("title", "S");
+                put("GlossList", new LinkedList<Object>() {{
+                    add(new LinkedHashMap<String, Object>() {{
+                        put("GlossEntry1", new LinkedHashMap<String, Object>() {{
+                            put("ID", "SGML");
+                            put("SortAs", "SGML");
+                            put("GlossTerm", "Standard Generalized Markup Language");
+                            put("Acronym", "SGML");
+                            put("Abbrev", "ISO 8879:1986");
+                            put("GlossDef", new LinkedHashMap<String, Object>() {{
+                                put("para", "A meta-markup language, used to create markup languages such as DocBook.");
+                                put("GlossSeeAlso", new LinkedList<Object>() {{
+                                    add("GML");
+                                    add("XML");
+                                }});
+                            }});
+                            put("GlossSee", "markup");
+                        }});
+                        put("GlossEntry2", new LinkedHashMap<String, Object>() {{
+                            put("ID", "SQLM");
+                            put("SortAs", "SQLM");
+                            put("GlossTerm", "Standard Query Language Markup");
+                            put("Acronym", "SQLM");
+                            put("Abbrev", "ISO 8109:4482");
+                            put("GlossDef", new LinkedHashMap<String, Object>() {{
+                                put("para", "A query language used for databases such as MySql and PostgreSQL");
+                                put("GlossSeeAlso", new LinkedList<Object>() {{
+                                    add("SQL");
+                                    add("noSQL");
+                                }});
+                            }});
+                            put("GlossSee", "query");
+                        }});
+                    }});
+                }});
+            }});
+        }});
+    }};
 
     private MtxJsonParser mtxJsonParser;
 
