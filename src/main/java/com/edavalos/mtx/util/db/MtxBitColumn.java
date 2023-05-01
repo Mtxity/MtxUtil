165220dcff2ae6fdb84b7dd504a5d67b0c097da5
Enrique <edavalos@ucsc.edu>
2023-05-01 00:33:08 -0700

    Added MtxBitColumn method skeletons

--- src/main/java/com/edavalos/mtx/util/db/MtxBitColumn.java
+++ src/main/java/com/edavalos/mtx/util/db/MtxBitColumn.java
@@ -1,7 +1,17 @@
 package com.edavalos.mtx.util.db;
 
+/**
+ * Bit columns are a way to store multiple booleans in a database without needing a separate column for each boolean.
+ * Instead of 32 separate columns, only one integer column is needed, and every bit used to store this integer holds a
+ * value representing some boolean. The integer's value itself is useless, as it is only a placeholder for bits.
+ * <p> </p>
+ * This data structure can be treated like a 32 bit boolean array, except the data is stored is an int, ready to be
+ * imported and exported into a database.
+ */
 public final class MtxBitColumn {
     private int value;
+
+    // ----------------------- Constructors ------------------------
 
     public MtxBitColumn() {
         this(0);
@@ -12,6 +22,76 @@ public final class MtxBitColumn {
     }
 
     public MtxBitColumn(boolean[] bitArray) {
+        //
+    }
+
+    // ---------------------- Public Methods -----------------------
+
+    /**
+     * Sets the value of a bit at the specified column / index
+     * @param column Index of bit to change (zero indexed)
+     * @param newValue New value of bit
+     * @return Old value of bit at this column
+     */
+    public boolean setBit(int column, boolean newValue) {
+        //
+    }
+
+    /**
+     * Gets the value of a bit at the specified column / index
+     * @param column Index of bit to look up (zero indexed)
+     * @return Value of bit at this column
+     */
+    public boolean getBit(int column) {
+        //
+    }
+
+    /**
+     * Gets the database-friendly version of this bit column
+     * @return Integer used to store bits
+     */
+    public int getAllBits() {
+        return this.value;
+    }
+
+    /**
+     * Returns a boolean array representation of this bit column
+     * @return Boolean array of length 32 with every bit's value
+     */
+    public boolean[] getAllBitsAsArray() {
+        //
+    }
+
+    /**
+     * Overwrites the integer representing this bit column. Typically used when reading from a database
+     * @param value Integer used to store bits
+     */
+    public void setAllBits(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Overwrites the integer representing this bit column with an array holding 32 booleans
+     * @param bitArray boolean array of size 32
+     */
+    public void setAllBits(boolean[] bitArray) {
+        //
+    }
+
+    /**
+     * Sets all bits in this bit column to false
+     */
+    public void clear() {
+        this.value = 0;
+    }
+
+    // ---------------------- Private Methods ----------------------
+
+    private int convertBooleanArrayToInt(boolean[] booleans) {
+        //
+    }
+
+    private boolean[] convertIntToBooleanArray(int i) {
         //
     }
 }
