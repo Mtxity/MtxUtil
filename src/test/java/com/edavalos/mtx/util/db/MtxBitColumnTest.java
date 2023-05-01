36627244a143b93a6ea7d5059510b746b988413b
Enrique <edavalos@ucsc.edu>
2023-05-01 01:52:37 -0700

    Added proof-of-concept unit test for getBit() and setBit()

---
+++ src/test/java/com/edavalos/mtx/util/db/MtxBitColumnTest.java
@@ -0,0 +1,38 @@
+package com.edavalos.mtx.util.db;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public final class MtxBitColumnTest {
+    private MtxBitColumn mtxBitColumn;
+
+    @BeforeEach
+    public void setUp() {
+        mtxBitColumn = new MtxBitColumn();
+    }
+
+    @Test
+    public void testSetAndGetBit() {
+        for (int i = 0; i < 31; i++) {
+            assertFalse(mtxBitColumn.getBit(i));
+        }
+
+        int[] bitsToTest = new int[] {3, 6, 10, 17, 24, 25, 30};
+        for (int bitToTest : bitsToTest) {
+            assertFalse(mtxBitColumn.setBit(bitToTest, true));
+        }
+
+        for (int i = 0; i < 31; i++) {
+            if (Arrays.binarySearch(bitsToTest, i) < 0) {
+                assertFalse(mtxBitColumn.getBit(i));
+            } else {
+                assertTrue(mtxBitColumn.getBit(i));
+            }
+        }
+    }
+}
