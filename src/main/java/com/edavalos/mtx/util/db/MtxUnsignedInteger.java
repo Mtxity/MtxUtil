6fb67b754374baddfce53f8c8ea18ccf35f53d61
Enrique <edavalos@ucsc.edu>
2023-05-08 01:50:08 -0700

    Added compare() and parseUnsigned() to MtxUnsignedInteger
    Also edited javadocs

--- src/main/java/com/edavalos/mtx/util/db/MtxUnsignedInteger.java
+++ src/main/java/com/edavalos/mtx/util/db/MtxUnsignedInteger.java
@@ -1,5 +1,8 @@
 package com.edavalos.mtx.util.db;
 
+/**
+ * A wrapper for unsigned integers.
+ */
 public final class MtxUnsignedInteger extends Number implements Comparable<MtxUnsignedInteger> {
     private final int value;
 
@@ -84,8 +87,8 @@ public final class MtxUnsignedInteger extends Number implements Comparable<MtxUn
     /**
      * Compares two MtxUnsignedInteger objects numerically.
      *
-     * @param  anotherMtxUnsignedInteger the MtxUnsignedInteger to be
-     *                                   compared.
+     * @param anotherMtxUnsignedInteger the MtxUnsignedInteger to be
+     *                                  compared.
      * @return the value {@code 0} if this MtxUnsignedInteger is
      *         equal to the argument MtxUnsignedInteger, a value less
      *         than {@code 0} if this MtxUnsignedInteger is
@@ -133,6 +136,35 @@ public final class MtxUnsignedInteger extends Number implements Comparable<MtxUn
     @Override
     public double doubleValue() {
         return ((double) this.value);
+    }
+
+    // ---------------------- Static Methods -----------------------
+
+    /**
+     * Runs {@link java.lang.Integer#compare(int, int)} as if
+     * x and y were their absolute values.
+     * @param x the first int to compare
+     * @param y the second int to compare
+     * @return 0 if abs(x) == abs(y), value greater than 0 if
+     *         abs(x) > abs(y), value less than 0 otherwise.
+     */
+    public static int compare(int x, int y) {
+        x = Math.abs(x);
+        y = Math.abs(y);
+        return Integer.compare(x, y);
+    }
+
+    /**
+     * Parses the string argument as an unsigned decimal integer.
+     * The characters in the string must all be decimal digits.
+     * @param s a String to be parsed.
+     * @return an MtxUnsignedInteger represented by the argument
+     * @throws NumberFormatException if argument String contains
+     *         any characters other than digits (for example, if
+     *         string contains decimal points or negative signs)
+     */
+    public static MtxUnsignedInteger parseUnsignedInt(String s) {
+        return new MtxUnsignedInteger(s);
     }
 
     // ---------------------- Private Methods ----------------------
