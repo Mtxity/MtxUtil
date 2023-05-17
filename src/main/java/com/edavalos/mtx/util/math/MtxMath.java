5750a80abfcf2109b25e499e3987a0ad4ee49991
Enrique Davalos <edavalos@ucsc.edu>
2023-05-17 01:06:44 -0700

    Added isPrime() to MtxMath

--- src/main/java/com/edavalos/mtx/util/math/MtxMath.java
+++ src/main/java/com/edavalos/mtx/util/math/MtxMath.java
@@ -103,4 +103,17 @@ public final class MtxMath {
             return fibonacci;
         }
     }
+
+    public static boolean isPrime(int i) {
+        if (i <= 1) {
+            return false;
+        }
+
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
