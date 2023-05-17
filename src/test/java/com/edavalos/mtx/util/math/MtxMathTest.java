4fcb392e38ae6320dbbac47c89c3de2f249a4418
Enrique Davalos <edavalos@ucsc.edu>
2023-05-17 00:54:42 -0700

    Added unit tests to fibonacci methods

--- src/test/java/com/edavalos/mtx/util/math/MtxMathTest.java
+++ src/test/java/com/edavalos/mtx/util/math/MtxMathTest.java
@@ -65,4 +65,46 @@ public class MtxMathTest {
             assertEquals(errorMsg, error.getMessage());
         }
     }
+
+    @Nested
+    class FibonacciTests {
+        private static final List<Integer> SAMPLE_FIBONACCIS = new ArrayList<>() {
+            {
+                add(1);
+                add(1);
+                add(2);
+                add(3);
+                add(5);
+                add(8);
+                add(13);
+                add(21);
+            }
+        };
+
+        @Test
+        public void testIterator() {
+            List<Integer> actualElements = new ArrayList<>();
+            for (int i : new MtxMath.MtxFibonacci()) {
+                actualElements.add(i);
+            }
+
+            assertEquals(SAMPLE_FIBONACCIS, actualElements);
+        }
+
+        @Test
+        public void testFibonacci() {
+            for (int i = 1; i < SAMPLE_FIBONACCIS.size(); i++) {
+                assertEquals(Long.valueOf(SAMPLE_FIBONACCIS.get(i - 1)),  MtxMath.MtxFibonacci.fibonacci(i));
+            }
+        }
+
+        @Test
+        public void testFibonacci_negative() {
+            String errorMsg = "Invalid input. Fibonacci sequence starts from index 1.";
+            int sampleNegativeInteger = -46;
+
+            Throwable error = assertThrows(IllegalArgumentException.class, () -> MtxMath.MtxFibonacci.fibonacci(sampleNegativeInteger));
+            assertEquals(errorMsg, error.getMessage());
+        }
+    }
 }
