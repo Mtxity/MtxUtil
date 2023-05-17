fbaae482ddd096b75adec4f1d56efc9abf1da481
Enrique Davalos <edavalos@ucsc.edu>
2023-05-17 00:54:17 -0700

    Added fibonacci sequence to MtxMath

--- src/main/java/com/edavalos/mtx/util/math/MtxMath.java
+++ src/main/java/com/edavalos/mtx/util/math/MtxMath.java
@@ -3,11 +3,11 @@ package com.edavalos.mtx.util.math;
 import java.util.Iterator;
 
 public final class MtxMath {
+    private static final int DEFAULT_SIZE = 8;
     public static final int POSITIVE_INFINITY = (int) Double.POSITIVE_INFINITY;
     public static final int NEGATIVE_INFINITY = (int) Double.NEGATIVE_INFINITY;
 
     public static class MtxFactorial implements Iterable<Integer> {
-        private static final int DEFAULT_SIZE = 8;
         private final int iteratorLength;
 
         public MtxFactorial(int size) {
@@ -48,6 +48,59 @@ public final class MtxMath {
             }
 
             return factorial;
+        }
+    }
+
+    public static class MtxFibonacci implements Iterable<Integer> {
+        private final int iteratorLength;
+
+        public MtxFibonacci(int size) {
+            this.iteratorLength = size;
+        }
+
+        public MtxFibonacci() {
+            this(DEFAULT_SIZE);
+        }
+
+        @Override
+        public Iterator<Integer> iterator() {
+            return new Iterator<>() {
+                private int iterations = 1;
+
+                @Override
+                public boolean hasNext() {
+                    return this.iterations <= iteratorLength;
+                }
+
+                @Override
+                public Integer next() {
+                    int currentVal = fibonacci(this.iterations);
+                    this.iterations++;
+                    return currentVal;
+                }
+            };
+        }
+
+        public static int fibonacci(int n) {
+            if (n <= 0) {
+                throw new IllegalArgumentException("Invalid input. Fibonacci sequence starts from index 1.");
+            }
+
+            if (n == 1 || n == 2) {
+                return 1;
+            }
+
+            int fib1 = 1;
+            int fib2 = 1;
+            int fibonacci = 0;
+
+            for (int i = 3; i <= n; i++) {
+                fibonacci = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fibonacci;
+            }
+
+            return fibonacci;
         }
     }
 }
