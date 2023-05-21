c45e3291ed7be79432b53f93f650df510bfd5211
Enrique Davalos <edavalos@ucsc.edu>
2023-05-21 02:14:10 -0700

    Added getRomanNumeralFromInteger() to MtxRomanNumeral

--- src/main/java/com/edavalos/mtx/util/math/MtxRomanNumeral.java
+++ src/main/java/com/edavalos/mtx/util/math/MtxRomanNumeral.java
@@ -1,20 +1,38 @@
 package com.edavalos.mtx.util.math;
 
+import com.edavalos.mtx.util.map.MtxBiMap;
+import com.edavalos.mtx.util.map.MtxHashBiMap;
+
+import java.util.TreeSet;
+
 public class MtxRomanNumeral {
-    private static final MtxBiMap<Integer, String> ROMAN_LITERALS = new MtxBiMap<>();
-    static {
-        ROMAN_LITERALS.put(1000, "M");
-        ROMAN_LITERALS.put(900, "CM");
-        ROMAN_LITERALS.put(500, "D");
-        ROMAN_LITERALS.put(400, "CD");
-        ROMAN_LITERALS.put(100, "C");
-        ROMAN_LITERALS.put(90, "XC");
-        ROMAN_LITERALS.put(50, "L");
-        ROMAN_LITERALS.put(40, "XL");
-        ROMAN_LITERALS.put(10, "X");
-        ROMAN_LITERALS.put(9, "IX");
-        ROMAN_LITERALS.put(5, "V");
-        ROMAN_LITERALS.put(4, "IV");
-        ROMAN_LITERALS.put(1, "I");
+    private static final MtxBiMap<Integer, String> ROMAN_LITERALS = new MtxHashBiMap<>() {{
+        put(1000, "M");
+        put(900, "CM");
+        put(500, "D");
+        put(400, "CD");
+        put(100, "C");
+        put(90, "XC");
+        put(50, "L");
+        put(40, "XL");
+        put(10, "X");
+        put(9, "IX");
+        put(5, "V");
+        put(4, "IV");
+        put(1, "I");
+    }};
+    private static final TreeSet<Integer> ROMAN_NUMERAL_INTERVALS = new TreeSet<>(ROMAN_LITERALS.keySet());
+
+    // Source: https://codingnconcepts.com/java/integer-to-roman/
+    public static String getRomanNumeralFromInteger(int number) {
+        Integer l = ROMAN_NUMERAL_INTERVALS.floor(number);
+        if (l == null) {
+            return null;
+        }
+
+        if (number == l) {
+            return ROMAN_LITERALS.get(number);
+        }
+        return ROMAN_LITERALS.get(l) + getRomanNumeralFromInteger(number - l);
     }
 }
