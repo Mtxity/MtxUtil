6bf2dabca3f97ab9803342a0a9f8f6acf0d8f888
Enrique <edavalos@ucsc.edu>
2023-05-30 22:34:56 -0700

    Added getRelativeTimeToNow() to MtxTimeUtil

--- src/main/java/com/edavalos/mtx/util/time/MtxTimeUtil.java
+++ src/main/java/com/edavalos/mtx/util/time/MtxTimeUtil.java
@@ -1,5 +1,7 @@
 package com.edavalos.mtx.util.time;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.regex.Pattern;
 
 public final class MtxTimeUtil {
@@ -35,5 +37,62 @@ public final class MtxTimeUtil {
         return (time < 10) ?
                 "0" + time :
                 String.valueOf(time);
+    }
+
+    // Source: https://stackoverflow.com/a/25760725
+    public static String getRelativeTimeToNow(LocalDateTime otherTime) {
+        LocalDateTime now = LocalDateTime.now();
+        boolean isInFuture = otherTime.isAfter(now);
+
+        LocalDateTime fromDateTime, toDateTime;
+        if (isInFuture) {
+            fromDateTime = now;
+            toDateTime = otherTime;
+        } else {
+            fromDateTime = otherTime;
+            toDateTime = now;
+        }
+
+        LocalDateTime tempDateTime = LocalDateTime.from(fromDateTime);
+
+        long years = tempDateTime.until(toDateTime, ChronoUnit.YEARS);
+        tempDateTime = tempDateTime.plusYears(years);
+
+        long months = tempDateTime.until(toDateTime, ChronoUnit.MONTHS);
+        tempDateTime = tempDateTime.plusMonths(months);
+
+        long days = tempDateTime.until(toDateTime, ChronoUnit.DAYS);
+        tempDateTime = tempDateTime.plusDays(days);
+
+        long hours = tempDateTime.until(toDateTime, ChronoUnit.HOURS);
+        tempDateTime = tempDateTime.plusHours(hours);
+
+        long minutes = tempDateTime.until(toDateTime, ChronoUnit.MINUTES);
+        tempDateTime = tempDateTime.plusMinutes(minutes);
+
+        long seconds = tempDateTime.until(toDateTime, ChronoUnit.SECONDS);
+
+        String summarizedTimeLength;
+        if (years > 0) {
+            summarizedTimeLength = years + " year" + (years > 1 ? "s" : "");
+        } else if (months > 0) {
+            summarizedTimeLength = months + " month" + (months > 1 ? "s" : "");
+        } else if (days > 0) {
+            summarizedTimeLength = days + " day" + (days > 1 ? "s" : "");
+        } else if (hours > 0) {
+            summarizedTimeLength = hours + " hour" + (hours > 1 ? "s" : "");
+        } else if (minutes > 0) {
+            summarizedTimeLength = minutes + " minute" + (minutes > 1 ? "s" : "");
+        } else if (seconds > 0) {
+            summarizedTimeLength = seconds + " second" + (seconds > 1 ? "s" : "");
+        } else {
+            return "now";
+        }
+
+        if (isInFuture) {
+            return "in " + summarizedTimeLength;
+        } else {
+            return summarizedTimeLength + " ago";
+        }
     }
 }
