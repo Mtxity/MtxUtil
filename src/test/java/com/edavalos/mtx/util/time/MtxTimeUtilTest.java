489883b2926b2f42b3571917f96520d294b6baa3
Enrique <edavalos@ucsc.edu>
2023-05-30 21:58:56 -0700

    Added skeleton unit tests for getRelativeTimeToNow()
    Setting up testing for a future method in MtxTimeUtil

--- src/test/java/com/edavalos/mtx/util/time/MtxTimeUtilTest.java
+++ src/test/java/com/edavalos/mtx/util/time/MtxTimeUtilTest.java
@@ -108,4 +108,152 @@ public final class MtxTimeUtilTest {
             }
         }
     }
+
+    @Nested
+    class GetRelativeTimeToNowTests {
+
+        @Nested
+        class GetFutureRelativeTimeToNowTests {
+
+            @Nested
+            class GetFutureSingularRelativeTimeToNowTests {
+
+                @Test
+                public void testGetRelativeTimeToNow_future_singular_years() {
+                    String expected = "in 1 year";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_singular_months() {
+                    String expected = "in 1 month";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_singular_days() {
+                    String expected = "in 1 day";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_singular_hours() {
+                    String expected = "in 1 hour";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_singular_minutes() {
+                    String expected = "in 1 minute";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_singular_seconds() {
+                    String expected = "in 1 second";
+                }
+            }
+
+            @Nested
+            class GetFuturePluralRelativeTimeToNowTests {
+
+                @Test
+                public void testGetRelativeTimeToNow_future_plural_years() {
+                    String expected = "in 2 years";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_plural_months() {
+                    String expected = "in 10 months";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_plural_days() {
+                    String expected = "in 5 days";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_plural_hours() {
+                    String expected = "in 9 hours";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_plural_minutes() {
+                    String expected = "in 32 minutes";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_future_plural_seconds() {
+                    String expected = "in 50 seconds";
+                }
+            }
+        }
+
+        @Nested
+        class GetPastRelativeTimeToNowTests {
+
+            @Nested
+            class GetPastSingularRelativeTimeToNowTests {
+
+                @Test
+                public void testGetRelativeTimeToNow_past_singular_years() {
+                    String expected = "1 year ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_singular_months() {
+                    String expected = "1 month ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_singular_days() {
+                    String expected = "1 day ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_singular_hours() {
+                    String expected = "1 hour ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_singular_minutes() {
+                    String expected = "1 minute ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_singular_seconds() {
+                    String expected = "1 second ago";
+                }
+            }
+
+            @Nested
+            class GetPastPluralRelativeTimeToNowTests {
+
+                @Test
+                public void testGetRelativeTimeToNow_past_plural_years() {
+                    String expected = "2 years ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_plural_months() {
+                    String expected = "10 months ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_plural_days() {
+                    String expected = "5 days ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_plural_hours() {
+                    String expected = "9 hours ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_plural_minutes() {
+                    String expected = "32 minutes ago";
+                }
+
+                @Test
+                public void testGetRelativeTimeToNow_past_plural_seconds() {
+                    String expected = "50 seconds ago";
+                }
+            }
+        }
+    }
 }
