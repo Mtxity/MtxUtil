e5de2d531fbb993fb7532b38efc0b50e8ee961fd
Enrique <edavalos@ucsc.edu>
2023-05-30 22:35:18 -0700

    Added unit tests for getRelativeTimeToNow()

--- src/test/java/com/edavalos/mtx/util/time/MtxTimeUtilTest.java
+++ src/test/java/com/edavalos/mtx/util/time/MtxTimeUtilTest.java
@@ -3,6 +3,9 @@ package com.edavalos.mtx.util.time;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -121,31 +124,53 @@ public final class MtxTimeUtilTest {
                 @Test
                 public void testGetRelativeTimeToNow_future_singular_years() {
                     String expected = "in 1 year";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(1, ChronoUnit.YEARS)
+                                              .plus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_singular_months() {
                     String expected = "in 1 month";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(1, ChronoUnit.MONTHS)
+                                              .plus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_singular_days() {
                     String expected = "in 1 day";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(1, ChronoUnit.DAYS)
+                                              .plus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_singular_hours() {
                     String expected = "in 1 hour";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(1, ChronoUnit.HOURS)
+                                              .plus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_singular_minutes() {
                     String expected = "in 1 minute";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(2, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_singular_seconds() {
                     String expected = "in 1 second";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(2, ChronoUnit.SECONDS);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
             }
 
@@ -155,31 +180,53 @@ public final class MtxTimeUtilTest {
                 @Test
                 public void testGetRelativeTimeToNow_future_plural_years() {
                     String expected = "in 2 years";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(2, ChronoUnit.YEARS)
+                                              .plus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_plural_months() {
                     String expected = "in 10 months";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(10, ChronoUnit.MONTHS)
+                                              .plus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_plural_days() {
                     String expected = "in 5 days";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                             .plus(5, ChronoUnit.DAYS)
+                                             .plus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_plural_hours() {
                     String expected = "in 9 hours";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                             .plus(9, ChronoUnit.HOURS)
+                                             .plus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_plural_minutes() {
                     String expected = "in 32 minutes";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(33, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_future_plural_seconds() {
                     String expected = "in 50 seconds";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .plus(51, ChronoUnit.SECONDS);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
             }
         }
@@ -193,31 +240,53 @@ public final class MtxTimeUtilTest {
                 @Test
                 public void testGetRelativeTimeToNow_past_singular_years() {
                     String expected = "1 year ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(1, ChronoUnit.YEARS)
+                                              .minus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_singular_months() {
                     String expected = "1 month ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(1, ChronoUnit.MONTHS)
+                                              .minus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_singular_days() {
                     String expected = "1 day ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(1, ChronoUnit.DAYS)
+                                              .minus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_singular_hours() {
                     String expected = "1 hour ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(1, ChronoUnit.HOURS)
+                                              .minus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_singular_minutes() {
                     String expected = "1 minute ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_singular_seconds() {
                     String expected = "1 second ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(1, ChronoUnit.SECONDS);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
             }
 
@@ -227,33 +296,61 @@ public final class MtxTimeUtilTest {
                 @Test
                 public void testGetRelativeTimeToNow_past_plural_years() {
                     String expected = "2 years ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(2, ChronoUnit.YEARS)
+                                              .minus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_plural_months() {
                     String expected = "10 months ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(10, ChronoUnit.MONTHS)
+                                              .minus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_plural_days() {
                     String expected = "5 days ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(5, ChronoUnit.DAYS)
+                                              .minus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_plural_hours() {
                     String expected = "9 hours ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(9, ChronoUnit.HOURS)
+                                              .minus(1, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_plural_minutes() {
                     String expected = "32 minutes ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(32, ChronoUnit.MINUTES);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
 
                 @Test
                 public void testGetRelativeTimeToNow_past_plural_seconds() {
                     String expected = "50 seconds ago";
+                    LocalDateTime otherTime = LocalDateTime.now()
+                                              .minus(50, ChronoUnit.SECONDS);
+                    assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(otherTime));
                 }
             }
+        }
+
+        @Test
+        public void testGetRelativeTimeToNow_now() {
+            String expected = "now";
+            assertEquals(expected, MtxTimeUtil.getRelativeTimeToNow(LocalDateTime.now()));
         }
     }
 }
