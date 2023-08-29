4faed70980fcf199410356b1df3222dadb6c0944
Enrique <edavalos@ucsc.edu>
2023-08-29 00:56:53 -0700

    Added toString() to MtxMatrix

--- src/main/java/com/edavalos/mtx/util/grouping/MtxMatrix.java
+++ src/main/java/com/edavalos/mtx/util/grouping/MtxMatrix.java
@@ -1,5 +1,9 @@
 package com.edavalos.mtx.util.grouping;
 
+import com.edavalos.mtx.util.string.MtxStringUtil;
+
+import java.util.regex.Pattern;
+
 public class MtxMatrix {
     private final int rows;
     private final int cols;
@@ -29,5 +33,43 @@ public class MtxMatrix {
         this.matrix = rows;
         this.rows = rows.length;
         this.cols = columns;
+    }
+
+    @Override
+    public String toString() {
+        if (this.rows == 0) {
+            return "[ ]";
+        }
+
+        int maxLen = 0;
+        int maxEnd = 0;
+        for (int i = 0; i < this.rows; i++) {
+            int intLen;
+            for (int j = 0; j < this.cols; j++) {
+                intLen = String.valueOf(this.matrix[i][j]).length();
+                if (intLen > maxLen) {
+                    maxLen = intLen;
+                }
+            }
+            intLen = String.valueOf(this.matrix[i][this.cols - 1]).length();
+            if (intLen > maxEnd) {
+                maxEnd = intLen;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < this.rows; i++) {
+            sb.append("  [");
+            for (int j = 0; j < this.cols - 1; j++) {
+                sb.append(MtxStringUtil.rightPad(this.matrix[i][j] + ", ", maxLen + 2));
+            }
+            sb.append(MtxStringUtil.rightPad(String.valueOf(this.matrix[i][this.cols - 1]), maxEnd));
+            sb.append("],\n");
+        }
+        sb.append("eod");
+
+        return sb.toString()
+                 .replaceFirst(Pattern.quote("  ["), "[ [")
+                 .replace("],\neod", "] ]");
     }
 }
