462c466bf4364f22f71c1b6a16dec853ae7e638a
Enrique <edavalos@ucsc.edu>
2023-09-09 00:54:59 -0700

    Added MtxLimitedEditsVar

---
+++ src/main/java/com/edavalos/mtx/util/grouping/MtxLimitedEditsVar.java
@@ -0,0 +1,44 @@
+package com.edavalos.mtx.util.grouping;
+
+/**
+ * This data structure is identical to MtxVersionedVar, except it only allows its value to be changed n times, after
+ * which it becomes immutable. n is specified in the constructor.
+ * @param <T>
+ */
+public class MtxLimitedEditsVar<T> extends MtxVersionedVar<T> {
+    protected static final String ERROR_NO_EDITS_LEFT = "This value can only be set %s times, and there are none left!";
+
+    private final int editsAllowed;
+    private int editsLeft;
+
+    // ----------------------- Constructors ------------------------
+
+    public MtxLimitedEditsVar(int totalEditsAllowed) {
+        this.editsAllowed = totalEditsAllowed;
+        this.editsLeft = totalEditsAllowed;
+    }
+
+    // ---------------------- Public Methods -----------------------
+
+    @Override
+    public T setValue(T newValue) {
+        if (newValue == null) {
+            throw new NullPointerException(ERROR_NULL);
+        }
+
+        if (this.editsLeft == 0) {
+            throw new UnsupportedOperationException(String.format(ERROR_NO_EDITS_LEFT, this.editsAllowed));
+        }
+
+        this.editsLeft --;
+        return super.setValue(newValue);
+    }
+
+    public int totalEditsAllowed() {
+        return this.editsAllowed;
+    }
+
+    public int editsLeft() {
+        return this.editsLeft;
+    }
+}
