a365f56a3ae2e9c7ea20729678bd2bdbe1207df2
Enrique <edavalos@ucsc.edu>
2023-09-19 23:58:56 -0700

    Added getRow() to MtxThreeColumnTable

--- src/main/java/com/edavalos/mtx/util/grouping/table/MtxThreeColumnTable.java
+++ src/main/java/com/edavalos/mtx/util/grouping/table/MtxThreeColumnTable.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MtxThreeColumnTable<A, B, C> {
-    protected record MtxTriple<a, b, c>(a first, b second, c third) {}
+    public record MtxTriple<a, b, c>(a first, b second, c third) {}
 
     private final HashMap<A, MtxTriple<A, B, C>> rowsMap;
     private final ArrayList<MtxTriple<A, B, C>> rowsList;
@@ -22,5 +22,9 @@ public class MtxThreeColumnTable<A, B, C> {
         MtxTriple<A, B, C> newRow = new MtxTriple<>(primaryKey, column1, column2);
         this.rowsMap.put(primaryKey, newRow);
         this.rowsList.add(newRow);
+    }
+
+    public MtxTriple<A, B, C> getRow(A primaryKey) {
+        return this.rowsMap.get(primaryKey);
     }
 }
