b83e19e876793892a3af2aac913ea64ae554ad59
Enrique <edavalos@ucsc.edu>
2024-03-10 01:09:22 -0800

    Added MtxRandom methods

--- src/main/java/com/edavalos/mtx/util/generator/MtxRandom.java
+++ src/main/java/com/edavalos/mtx/util/generator/MtxRandom.java
@@ -8,4 +8,37 @@ public abstract class MtxRandom {
     public MtxRandom(long seed) {
         this.random = new Random(seed);
     }
+
+    public byte randomByte() {
+        long val = this.randomLong();
+        return Long.valueOf(val).byteValue();
+    }
+
+    public short randomShort() {
+        long val = this.randomLong();
+        return Long.valueOf(val).shortValue();
+    }
+
+    public int randomInt() {
+        long val = this.randomLong();
+        return Long.valueOf(val).intValue();
+    }
+
+    public long randomLong() {
+        return this.random.nextLong();
+    }
+
+    public float randomFloat() {
+        long val = this.randomLong();
+        return Long.valueOf(val).floatValue();
+    }
+
+    public double randomDouble() {
+        long val = this.randomLong();
+        return Long.valueOf(val).doubleValue();
+    }
+    public char randomChar() {
+        long val = this.randomLong();
+        return ((char) Long.valueOf(val).intValue());
+    }
 }
