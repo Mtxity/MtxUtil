b6e4bc69d84b5bae7601987bcbc777ae362340c5
Enrique <edavalos@ucsc.edu>
2024-03-26 23:13:46 -0700

    Added MtxValidatedVar

---
+++ src/main/java/com/edavalos/mtx/util/db/var/MtxValidatedVar.java
@@ -0,0 +1,44 @@
+package com.edavalos.mtx.util.db.var;
+
+public class MtxValidatedVar<T> {
+    protected static final String ERROR_MSG = "Cannot access value of invalid MtxValidatedVar";
+
+    private T value;
+    private boolean isValid;
+
+    // ----------------------- Constructors ------------------------
+
+    public MtxValidatedVar(T value) {
+        this.value = value;
+    }
+
+    public MtxValidatedVar() {
+        this.value = null;
+    }
+
+    // ---------------------- Public Methods -----------------------
+
+    public T getValue() {
+        if (this.isValid) {
+            return this.value;
+        } else {
+            throw new IllegalStateException(ERROR_MSG);
+        }
+    }
+
+    public void setValue(T newValue) {
+        this.value = newValue;
+    }
+
+    public boolean isValid() {
+        return this.isValid;
+    }
+
+    public void invalidate() {
+        this.isValid = false;
+    }
+
+    public void validate() {
+        this.isValid = true;
+    }
+}
