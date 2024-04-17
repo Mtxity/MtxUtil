f921e54e64c924dec912111f95730298b15025e5
Enrique <edavalos@ucsc.edu>
2024-04-16 22:42:47 -0700

    Added empty() and isEmpty() unit tests for MtxOptionalValue

--- src/test/java/com/edavalos/mtx/util/db/var/MtxOptionalVarTest.java
+++ src/test/java/com/edavalos/mtx/util/db/var/MtxOptionalVarTest.java
@@ -2,7 +2,12 @@ package com.edavalos.mtx.util.db.var;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MtxOptionalVarTest {
     private final String SAMPLE_STR = "SAMPLE_STRING";
@@ -14,5 +19,20 @@ public class MtxOptionalVarTest {
         mtxOVar = new MtxOptionalVar<>(SAMPLE_STR);
 
         assertEquals(SAMPLE_STR, mtxOVar.getValue());
+    }
+
+    @Test
+    public void testEmpty() {
+        mtxOVar = MtxOptionalVar.empty();
+
+        assertTrue(mtxOVar.isEmpty());
+        assertThrows(NoSuchElementException.class, () -> mtxOVar.getValue());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(new MtxOptionalVar<Integer>(null).isEmpty());
+        assertTrue(MtxOptionalVar.empty().isEmpty());
+        assertFalse(new MtxOptionalVar<Integer>(5).isEmpty());
     }
 }
