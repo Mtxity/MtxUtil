23f885392438fbca801bb82585e337193466a210
Enrique <edavalos@ucsc.edu>
2024-04-26 15:54:38 -0700

    Added constructors and from() to MtxSelectQuery

--- src/main/java/com/edavalos/mtx/util/db/query/MtxSelectQuery.java
+++ src/main/java/com/edavalos/mtx/util/db/query/MtxSelectQuery.java
@@ -1,13 +1,34 @@
 package com.edavalos.mtx.util.db.query;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MtxSelectQuery extends MtxQuery {
+    private String FROM;
     private final String SELECT;
-    private final String FROM;
     private final HashMap<String, String> INNER_JOIN;
     private final HashMap<String, String> OUTER_JOIN;
     private final List<String> WHERE;
     private final List<String> ORDERBY;
+    private boolean complete;
+
+    public MtxSelectQuery(String select) {
+        this.SELECT = select;
+        this.INNER_JOIN = new HashMap<>();
+        this.OUTER_JOIN = new HashMap<>();
+        this.WHERE = new LinkedList<>();
+        this.ORDERBY = new LinkedList<>();
+        this.complete = false;
+    }
+
+    public MtxSelectQuery() {
+        this("*");
+    }
+
+    public MtxSelectQuery from(String table) {
+        this.FROM = table;
+        return this;
+    }
+
 }
