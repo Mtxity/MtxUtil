01a92c39cd874f75e4d062d0c1828e4337856d9c
Enrique <edavalos@ucsc.edu>
2024-04-29 21:07:25 -0700

    Added Comparison class and added keyword to ComparisonOperator enum

--- src/main/java/com/edavalos/mtx/util/db/query/MtxQuery.java
+++ src/main/java/com/edavalos/mtx/util/db/query/MtxQuery.java
@@ -1,10 +1,41 @@
 package com.edavalos.mtx.util.db.query;
 
 public abstract class MtxQuery {
+    static class Comparison {
+        private final String leftVal;
+        private final ComparisonOperator operator;
+        private final String rightVal;
+
+        Comparison(String leftVal, ComparisonOperator operator, String rightVal) {
+            this.leftVal = leftVal;
+            this.rightVal = rightVal;
+            this.operator = operator;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + this.leftVal + " " + this.operator.getKeyword() + " " + this.rightVal + ")";
+        }
+    }
+
     enum ComparisonOperator {
-        EQUAL_TO, NOT_EQUAL_TO, GREATER_THAN, LESS_THAN,
-        GREATER_THAN_OR_EQUAL_TO, LESS_THAN_OR_EQUAL_TO,
-        LIKE, BETWEEN, AND, NOT, OR
+        EQUAL_TO("="), NOT_EQUAL_TO("<>"), GREATER_THAN(">"), LESS_THAN("<"),
+        GREATER_THAN_OR_EQUAL_TO(">="), LESS_THAN_OR_EQUAL_TO("<="),
+        LIKE, BETWEEN, AND, NOT, OR;
+
+        private final String keyword;
+
+        ComparisonOperator(String keyword) {
+            this.keyword = keyword;
+        }
+
+        ComparisonOperator() {
+            this.keyword = this.name().replaceAll("_", " ");
+        }
+
+        public String getKeyword() {
+            return this.keyword;
+        }
     }
 
     @Override
