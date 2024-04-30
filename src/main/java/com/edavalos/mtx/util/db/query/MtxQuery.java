e540ac4e68740cd869f937d73d651ecbb74fb4b7
Enrique <edavalos@ucsc.edu>
2024-04-29 20:55:53 -0700

    Added ComparisonOperator enums in MtxQuery

--- src/main/java/com/edavalos/mtx/util/db/query/MtxQuery.java
+++ src/main/java/com/edavalos/mtx/util/db/query/MtxQuery.java
@@ -1,6 +1,12 @@
 package com.edavalos.mtx.util.db.query;
 
 public abstract class MtxQuery {
+    enum ComparisonOperator {
+        EQUAL_TO, NOT_EQUAL_TO, GREATER_THAN, LESS_THAN,
+        GREATER_THAN_OR_EQUAL_TO, LESS_THAN_OR_EQUAL_TO,
+        LIKE, BETWEEN, AND, NOT, OR
+    }
+
     @Override
     public abstract String toString();
 }
