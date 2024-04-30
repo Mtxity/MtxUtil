034959cba3945d9cd644d061c57cf930a7f2b662
Enrique <edavalos@ucsc.edu>
2024-04-30 02:04:59 -0700

    Added primitive type append() methods to MtxStringBuilder

--- src/main/java/com/edavalos/mtx/util/string/MtxStringBuilder.java
+++ src/main/java/com/edavalos/mtx/util/string/MtxStringBuilder.java
@@ -17,16 +17,56 @@ public class MtxStringBuilder {
         this.parts.add(firstString);
     }
 
-
-    // ---------------------- Public Methods -----------------------
-
     public MtxStringBuilder(Object firstValue) {
         this.parts = new ArrayList<>();
         this.parts.add(firstValue.toString());
     }
 
+
+    // ---------------------- Public Methods -----------------------
+
     public MtxStringBuilder append(String nextString) {
         this.parts.add(nextString);
+        return this;
+    }
+
+    public MtxStringBuilder append(char nextChar) {
+        this.parts.add(String.valueOf(nextChar));
+        return this;
+    }
+
+    public MtxStringBuilder append(int nextInt) {
+        this.parts.add(String.valueOf(nextInt));
+        return this;
+    }
+
+    public MtxStringBuilder append(long nextLong) {
+        this.parts.add(String.valueOf(nextLong));
+        return this;
+    }
+
+    public MtxStringBuilder append(short nextShort) {
+        this.parts.add(String.valueOf(nextShort));
+        return this;
+    }
+
+    public MtxStringBuilder append(byte nextByte) {
+        this.parts.add(String.valueOf(nextByte));
+        return this;
+    }
+
+    public MtxStringBuilder append(double nextDouble) {
+        this.parts.add(String.valueOf(nextDouble));
+        return this;
+    }
+
+    public MtxStringBuilder append(float nextFloat) {
+        this.parts.add(String.valueOf(nextFloat));
+        return this;
+    }
+
+    public MtxStringBuilder append(boolean nextBool) {
+        this.parts.add(String.valueOf(nextBool));
         return this;
     }
 
