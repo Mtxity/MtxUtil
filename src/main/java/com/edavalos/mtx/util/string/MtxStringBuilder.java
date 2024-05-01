0f11d6d1961b01bb8c6bba0715333b623b34cd07
Enrique <edavalos@ucsc.edu>
2024-05-01 00:11:45 -0700

    Changed inner list type of MtxStringBuilder and added appendString() to handle it

--- src/main/java/com/edavalos/mtx/util/string/MtxStringBuilder.java
+++ src/main/java/com/edavalos/mtx/util/string/MtxStringBuilder.java
@@ -1,11 +1,12 @@
 package com.edavalos.mtx.util.string;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class MtxStringBuilder {
-    private final ArrayList<String> parts;
+    private final ArrayList<Character> parts;
 
 
     // ----------------------- Constructors ------------------------
@@ -16,64 +17,64 @@ public class MtxStringBuilder {
 
     public MtxStringBuilder(String firstString) {
         this.parts = new ArrayList<>();
-        this.parts.add(firstString);
+        this.appendString(firstString);
     }
 
     public MtxStringBuilder(Object firstValue) {
         this.parts = new ArrayList<>();
-        this.parts.add(firstValue.toString());
+        this.appendString(firstValue.toString());
     }
 
 
     // ---------------------- Public Methods -----------------------
 
     public MtxStringBuilder append(String nextString) {
-        this.parts.add(nextString);
+        this.appendString(nextString);
         return this;
     }
 
     public MtxStringBuilder append(char nextChar) {
-        this.parts.add(String.valueOf(nextChar));
+        this.parts.add(nextChar);
         return this;
     }
 
     public MtxStringBuilder append(int nextInt) {
-        this.parts.add(String.valueOf(nextInt));
+        this.appendString(String.valueOf(nextInt));
         return this;
     }
 
     public MtxStringBuilder append(long nextLong) {
-        this.parts.add(String.valueOf(nextLong));
+        this.appendString(String.valueOf(nextLong));
         return this;
     }
 
     public MtxStringBuilder append(short nextShort) {
-        this.parts.add(String.valueOf(nextShort));
+        this.appendString(String.valueOf(nextShort));
         return this;
     }
 
     public MtxStringBuilder append(byte nextByte) {
-        this.parts.add(String.valueOf(nextByte));
+        this.appendString(String.valueOf(nextByte));
         return this;
     }
 
     public MtxStringBuilder append(double nextDouble) {
-        this.parts.add(String.valueOf(nextDouble));
+        this.appendString(String.valueOf(nextDouble));
         return this;
     }
 
     public MtxStringBuilder append(float nextFloat) {
-        this.parts.add(String.valueOf(nextFloat));
+        this.appendString(String.valueOf(nextFloat));
         return this;
     }
 
     public MtxStringBuilder append(boolean nextBool) {
-        this.parts.add(String.valueOf(nextBool));
+        this.appendString(String.valueOf(nextBool));
         return this;
     }
 
     public MtxStringBuilder append(Object nextValue) {
-        this.parts.add(nextValue.toString());
+        this.appendString(nextValue.toString());
         return this;
     }
 
@@ -101,7 +102,8 @@ public class MtxStringBuilder {
 
     // ------------------ Private Helper Methods -------------------
 
-    private String temp() {
-        return "";
+    private void appendString(String nextString) {
+        Character[] charArray = nextString.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        this.parts.addAll(Arrays.asList(charArray));
     }
 }
