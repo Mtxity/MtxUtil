1b9553c99f7fac59da3da0f403fa9f535255c3d6
Enrique <edavalos@ucsc.edu>
2024-05-01 01:46:25 -0700

    Added unit tests for append() in MtxStringBuilder

--- src/test/java/com/edavalos/mtx/util/string/MtxStringBuilderTest.java
+++ src/test/java/com/edavalos/mtx/util/string/MtxStringBuilderTest.java
@@ -1,7 +1,11 @@
 package com.edavalos.mtx.util.string;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -11,6 +15,11 @@ public class MtxStringBuilderTest {
     private static final String STARTING_STR = "abcd_efg_hijk";
 
     private MtxStringBuilder mtxStringBuilder;
+
+    @BeforeEach
+    public void setUp() {
+        mtxStringBuilder = new MtxStringBuilder(STARTING_STR);
+    }
 
     @Nested
     class ConstructorTests {
@@ -34,6 +43,119 @@ public class MtxStringBuilderTest {
             mtxStringBuilder = new MtxStringBuilder(array);
             assertFalse(mtxStringBuilder.isEmpty());
             assertEquals(array.toString(), mtxStringBuilder.toString());
+        }
+    }
+
+    @Nested
+    class AppendTests {
+
+        @Test
+        public void testAppend_string_empty() {
+            mtxStringBuilder = mtxStringBuilder.append("");
+            assertEquals(STARTING_STR, mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_string_notEmpty() {
+            mtxStringBuilder = mtxStringBuilder.append("_lmnop");
+            assertEquals(STARTING_STR + "_lmnop", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_char() {
+            mtxStringBuilder = mtxStringBuilder.append('x');
+            assertEquals(STARTING_STR + "x", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_int() {
+            int a = 5;
+            mtxStringBuilder = mtxStringBuilder.append(a);
+            assertEquals(STARTING_STR + "5", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_long() {
+            long a = 5;
+            mtxStringBuilder = mtxStringBuilder.append(a);
+            assertEquals(STARTING_STR + "5", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_short() {
+            short a = 5;
+            mtxStringBuilder = mtxStringBuilder.append(a);
+            assertEquals(STARTING_STR + "5", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_byte() {
+            byte a = 5;
+            mtxStringBuilder = mtxStringBuilder.append(a);
+            assertEquals(STARTING_STR + "5", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_double() {
+            double a = 5.1;
+            mtxStringBuilder = mtxStringBuilder.append(a);
+            assertEquals(STARTING_STR + "5.1", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_float() {
+            float a = 5.1f;
+            mtxStringBuilder = mtxStringBuilder.append(a);
+            assertEquals(STARTING_STR + "5.1", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_boolean_true() {
+            mtxStringBuilder = mtxStringBuilder.append(true);
+            assertEquals(STARTING_STR + "true", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_boolean_false() {
+            mtxStringBuilder = mtxStringBuilder.append(false);
+            assertEquals(STARTING_STR + "false", mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_object() {
+            Map<Boolean, Boolean> a = Map.of(true, true, false, true);
+            mtxStringBuilder = mtxStringBuilder.append(a);
+            assertEquals(STARTING_STR + a, mtxStringBuilder.toString());
+        }
+
+        @Test
+        public void testAppend_objects_array() {
+            String[] arr = {"_", "l", "m", "n"};
+            mtxStringBuilder = mtxStringBuilder.append(arr);
+            assertEquals(
+                    STARTING_STR + arr[0] + arr[1] + arr[2] + arr[3],
+                    mtxStringBuilder.toString()
+            );
+        }
+
+        @Test
+        public void testAppend_objects_list() {
+            List<List<Integer>> mat = List.of(
+                    List.of(1, 2, 3),
+                    List.of(4, 5, 6),
+                    List.of(7, 8, 9)
+            );
+            mtxStringBuilder = mtxStringBuilder.append(mat);
+            assertEquals(
+                    STARTING_STR + mat.get(0) + mat.get(1) + mat.get(2),
+                    mtxStringBuilder.toString()
+            );
+        }
+
+        @Test
+        public void testAppend_objects_list_null() {
+            mtxStringBuilder = mtxStringBuilder.append((List<?>) null);
+            assertEquals(STARTING_STR, mtxStringBuilder.toString());
         }
     }
 }
