e9b62302a6ccd0c6f52ea77717504f184ab53ceb
Enrique <edavalos@ucsc.edu>
2024-05-02 23:31:09 -0700

    Added unit test for leftJoin() in MtxSelectQuery

--- src/test/java/com/edavalos/mtx/util/db/query/MtxSelectQueryTest.java
+++ src/test/java/com/edavalos/mtx/util/db/query/MtxSelectQueryTest.java
@@ -3,6 +3,7 @@ package com.edavalos.mtx.util.db.query;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.edavalos.mtx.util.db.query.MtxQuery.eq;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class MtxSelectQueryTest {
@@ -17,6 +18,16 @@ public final class MtxSelectQueryTest {
     public void testQuery_plain() {
         String expected = "SELECT * FROM table;";
         String actual = mtxSelectQuery.from("table").toString();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testQuery_leftJoin() {
+        String expected = "SELECT * FROM table LEFT JOIN otherTable ON (t = u);";
+        String actual = mtxSelectQuery
+                        .from("table")
+                        .leftJoin("otherTable", eq("t", "u"))
+                        .toString();
         assertEquals(expected, actual);
     }
 }
