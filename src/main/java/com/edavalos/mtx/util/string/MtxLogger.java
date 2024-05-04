c83f12bd1276270f97045bc0fd11925600ce6f1a
Enrique <edavalos@ucsc.edu>
2024-05-04 01:01:03 -0700

    Updated getTimestamp() in MtxLogger to use Date

--- src/main/java/com/edavalos/mtx/util/string/MtxLogger.java
+++ src/main/java/com/edavalos/mtx/util/string/MtxLogger.java
@@ -2,14 +2,15 @@ package com.edavalos.mtx.util.string;
 
 import java.sql.Timestamp;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MtxLogger {
     protected static String getTimestamp() {
-        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        Date ts = new Date();
         StringBuilder sb = new StringBuilder("[");
         sb.append(ts.getYear()).append('-');
-        sb.append(ts.getDate()).append('-');
-        sb.append(ts.getMonth());
+        sb.append(ts.getMonth()).append('-');
+        sb.append(ts.getDay());
 
         return sb.toString();
     }
