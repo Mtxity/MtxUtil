fab9b79549e7323e3b265293720b9feb836323ea
Enrique <edavalos@ucsc.edu>
2024-05-04 01:43:43 -0700

    Updated unit test for getTimestamp() in MtxLogger

--- src/test/java/com/edavalos/mtx/util/string/MtxLoggerTest.java
+++ src/test/java/com/edavalos/mtx/util/string/MtxLoggerTest.java
@@ -2,14 +2,16 @@ package com.edavalos.mtx.util.string;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MtxLoggerTest {
 
     @Test
     public void testGetTimestamp() {
-        String actualTs = MtxLogger.getTimestamp();
-        String expectedTs = "[2024-05-03 03:04 AM]";
-        assertEquals(actualTs, expectedTs);
+        Pattern pattern = Pattern.compile("\\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [AP]M]");
+        String ts = MtxLogger.getTimestamp();
+        assertTrue(ts.matches(pattern.pattern()));
     }
 }
