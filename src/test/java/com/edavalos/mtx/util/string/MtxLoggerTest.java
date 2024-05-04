d250cef40da011e6e98ec39b4520e6209279ece1
Enrique <edavalos@ucsc.edu>
2024-05-04 03:00:17 -0700

    Added unit tests for fillInBrackets() in MtxLogger

--- src/test/java/com/edavalos/mtx/util/string/MtxLoggerTest.java
+++ src/test/java/com/edavalos/mtx/util/string/MtxLoggerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.regex.Pattern;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MtxLoggerTest {
@@ -13,5 +15,21 @@ public class MtxLoggerTest {
         Pattern pattern = Pattern.compile("\\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [AP]M]");
         String ts = MtxLogger.getTimestamp();
         assertTrue(ts.matches(pattern.pattern()));
+    }
+
+    @Test
+    public void testFillInBrackets() {
+        String template = "Test {} {} {} ok";
+        String expected = "Test a b c ok";
+        String actual = MtxLogger.fillInBrackets(template, "a", "b", "c");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testFillInBrackets_incorrectAmountOfArgs() {
+        IllegalArgumentException e = assertThrows(
+                IllegalArgumentException.class,
+                () -> MtxLogger.fillInBrackets("{} {}", "1", "2", "3")
+        );
+        assertEquals("Number of arguments do not match log template! Expected 2 but found 3", e.getMessage());
     }
 }
