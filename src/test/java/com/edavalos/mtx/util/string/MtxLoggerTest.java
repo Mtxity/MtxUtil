0149f37998bb9ccae785f0c1d79fe64cfca51183
Enrique <edavalos@ucsc.edu>
2024-05-05 02:36:44 -0700

    Added unit tests for log() in MtxLogger

--- src/test/java/com/edavalos/mtx/util/string/MtxLoggerTest.java
+++ src/test/java/com/edavalos/mtx/util/string/MtxLoggerTest.java
@@ -1,14 +1,18 @@
 package com.edavalos.mtx.util.string;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.regex.Pattern;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MtxLoggerTest {
+    private MtxLogger mtxLogger;
 
     @Test
     public void testGetTimestamp() {
@@ -24,6 +28,7 @@ public class MtxLoggerTest {
         String actual = MtxLogger.fillInBrackets(template, "a", "b", "c");
         assertEquals(expected, actual);
     }
+
     @Test
     public void testFillInBrackets_incorrectAmountOfArgs() {
         IllegalArgumentException e = assertThrows(
@@ -31,5 +36,50 @@ public class MtxLoggerTest {
                 () -> MtxLogger.fillInBrackets("{} {}", "1", "2", "3")
         );
         assertEquals("Number of arguments do not match log template! Expected 2 but found 3", e.getMessage());
+    }
+
+    @Nested
+    class LogTests {
+        private static final class SampleLog implements MtxLogger.MtxCompatibleLogger {
+            private String log;
+
+            @Override
+            public void log(String statement) {
+                this.log = statement;
+            }
+
+            public String getLog() {
+                return this.log;
+            }
+        }
+
+        private final SampleLog testLog = new SampleLog();
+
+        @BeforeEach
+        public void setUp() {
+            mtxLogger = new MtxLogger();
+        }
+
+        @Test
+        public void testLog_noArgs_noLogger() {
+            assertDoesNotThrow(() -> mtxLogger.log("test"));
+        }
+
+        @Test
+        public void testLog_withArgs_noLogger() {
+            assertDoesNotThrow(() -> mtxLogger.log("test {}", "test"));
+        }
+
+        @Test
+        public void testLog_noArgs_customLogger() {
+            mtxLogger.log("test", testLog);
+            assertEquals(MtxLogger.getTimestamp() + " test", testLog.getLog());
+        }
+
+        @Test
+        public void testLog_withArgs_customLogger() {
+            mtxLogger.log("test {}", testLog, "test");
+            assertEquals(MtxLogger.getTimestamp() + " test test", testLog.getLog());
+        }
     }
 }
