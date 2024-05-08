2f263b82904e74c84c0482d5f735294baf143e6d
Enrique <edavalos@ucsc.edu>
2024-05-08 00:05:14 -0700

    Added MtxChecksumIterativeIdGenerator

---
+++ src/main/java/com/edavalos/mtx/util/db/MtxChecksumIterativeIdGenerator.java
@@ -0,0 +1,23 @@
+package com.edavalos.mtx.util.db;
+
+import java.util.Arrays;
+
+public class MtxChecksumIterativeIdGenerator extends MtxChecksumIdGenerator {
+    private int nextId;
+
+    public MtxChecksumIterativeIdGenerator(int length) {
+        super(length);
+    }
+
+    @Override
+    protected int[] getInitialDigits(int length) {
+        if (String.valueOf(this.nextId).length() > length) {
+            this.nextId = 0;
+        }
+        char[] digitChars = String.format("%0" + length + "d", this.nextId).toCharArray();
+        int[] digitInts = new int[length];
+        Arrays.setAll(digitInts, i -> Character.getNumericValue(digitChars[i]));
+        this.nextId ++;
+        return digitInts;
+    }
+}
