dda9a7f3b2c28756c38551c08b3da12ab404630b
Enrique <edavalos@ucsc.edu>
2024-05-09 20:59:58 -0700

    Added unit tests for getChecksumDigit() in MtxChecksumIdGenerator

--- src/test/java/com/edavalos/mtx/util/db/id/MtxChecksumIdGeneratorTest.java
+++ src/test/java/com/edavalos/mtx/util/db/id/MtxChecksumIdGeneratorTest.java
@@ -1,5 +1,6 @@
 package com.edavalos.mtx.util.db.id;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MtxChecksumIdGeneratorTest {
@@ -64,5 +66,48 @@ public class MtxChecksumIdGeneratorTest {
         mtxChecksumIdGenerator = new MtxChecksumIdGenerator(length);
 
         assertEquals(BigInteger.valueOf(10).pow(length), mtxChecksumIdGenerator.getTotalUniqueIds());
+    }
+
+    @Nested
+    public class TestGetChecksumDigit {
+
+        @Test
+        public void testGetChecksumDigit_knownExample() {
+            // From example in Wikipedia page for Luhn Algorithm
+            // https://en.wikipedia.org/wiki/Luhn_algorithm
+            int[] payload = {7,9,9,2,7,3,9,8,7,1,0};
+            int expected = 4;
+            assertEquals(expected, MtxChecksumIdGenerator.getChecksumDigit(payload));
+        }
+
+        @Test
+        public void testGetChecksumDigit_detectSwitchedDigits() {
+            int[] value1 = {1,3,5,7,9,0};
+            int[] value2 = {1,3,7,5,9,0};
+            assertNotEquals(
+                    MtxChecksumIdGenerator.getChecksumDigit(value1),
+                    MtxChecksumIdGenerator.getChecksumDigit(value2)
+            );
+        }
+
+        @Test
+        public void testGetChecksumDigit_detectDuplicateDigits() {
+            int[] value1 = {1,3,5,7,9,0};
+            int[] value2 = {1,1,3,5,7,9,0};
+            assertNotEquals(
+                    MtxChecksumIdGenerator.getChecksumDigit(value1),
+                    MtxChecksumIdGenerator.getChecksumDigit(value2)
+            );
+        }
+
+        @Test
+        public void testGetChecksumDigit_detectMissingDigits() {
+            int[] value1 = {1,3,5,7,9,0};
+            int[] value2 = {1,5,7,9,0};
+            assertNotEquals(
+                    MtxChecksumIdGenerator.getChecksumDigit(value1),
+                    MtxChecksumIdGenerator.getChecksumDigit(value2)
+            );
+        }
     }
 }
