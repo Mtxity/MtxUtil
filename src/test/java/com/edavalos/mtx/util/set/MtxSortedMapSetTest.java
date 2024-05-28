d269a8a1305a76dcfe35515140eec63c0fc67c58
Enrique <edavalos@ucsc.edu>
2024-05-28 00:49:30 -0700

    Added WIP unit tests for MtxSortedMapSet

---
+++ src/test/java/com/edavalos/mtx/util/set/MtxSortedMapSetTest.java
@@ -0,0 +1,19 @@
+package com.edavalos.mtx.util.set;
+
+import org.junit.jupiter.api.Test;
+
+public class MtxSortedMapSetTest {
+    private static final double[] VALS = {1.1, 2.2, 3.3};
+
+    private MtxSortedMapSet<Double> mtxSortedMapSet;
+
+    @Test
+    public void testConstructor_empty() {
+        //
+    }
+
+    @Test
+    public void testConstructor_withContents() {
+        //
+    }
+}
