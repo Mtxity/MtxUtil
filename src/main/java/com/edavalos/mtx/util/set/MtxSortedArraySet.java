4d00df0356e432aac4a420d0c6f90e0095c5b05c
Enrique <edavalos@ucsc.edu>
2024-05-30 23:36:29 -0700

    Added constructors to MtxSortedMapSet

--- src/main/java/com/edavalos/mtx/util/set/MtxSortedArraySet.java
+++ src/main/java/com/edavalos/mtx/util/set/MtxSortedArraySet.java
@@ -1,15 +1,27 @@
 package com.edavalos.mtx.util.set;
 
+import java.util.Comparator;
+
 /**
  * Ordered and auto-sorted implementation of MtxArraySet backed by a tree set.
  */
 public class MtxSortedArraySet<T> extends MtxArraySet<T> {
-    protected int next;
+    private final Comparator<T> comparator;
 
-    public MtxSortedArraySet() {
+    public MtxSortedArraySet(Comparator<T> comparator) {
+        super();
+        this.comparator = comparator;
     }
 
-    public MtxSortedArraySet(T... contents) {
+    public MtxSortedArraySet(Comparator<T> comparator, int startingSize) {
+        super(startingSize);
+        this.comparator = comparator;
+    }
+
+    public MtxSortedArraySet(Comparator<T> comparator, T... contents) {
+        super(contents);
+        this.comparator = comparator;
+        this.sort();
     }
 
     @Override
