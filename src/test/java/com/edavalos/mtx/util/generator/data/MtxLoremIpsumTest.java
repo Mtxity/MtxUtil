961f24ab3c252e995364cab8c8061ca682f7062f
Enrique <edavalos@ucsc.edu>
2024-06-08 00:02:49 -0700

    Added unit tests for multi-paragraph getWords() in MtxLoremIpsum

--- src/test/java/com/edavalos/mtx/util/generator/data/MtxLoremIpsumTest.java
+++ src/test/java/com/edavalos/mtx/util/generator/data/MtxLoremIpsumTest.java
@@ -89,6 +89,17 @@ public class MtxLoremIpsumTest {
         }
 
         @Test
+        public void testGetWords_110() {
+            String expected = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et odio pellentesque diam volutpat commodo sed egestas. Venenatis tellus in metus vulputate eu scelerisque felis. Vitae congue mauris rhoncus aenean vel. Nibh venenatis cras sed felis eget velit aliquet sagittis id. In dictum non consectetur a erat nam. Curabitur vitae nunc sed velit dignissim sodales ut eu. In pellentesque massa placerat duis ultricies lacus. Amet est placerat in egestas. Libero volutpat sed cras ornare. Adipiscing bibendum est ultricies integer quis auctor elit sed. Tempus imperdiet nulla malesuada pellentesque elit. Massa eget egestas purus viverra accumsan. Tincidunt id aliquet risus feugiat";
+
+            try {
+                assertEquals(expected, mtxLoremIpsum.getWords(110));
+            } catch (IOException e) {
+                fail(e.getMessage());
+            }
+        }
+
+        @Test
         public void testGetWords_tooSmall() {
             assertEquals(
                     "Minimum number of lorem ipsum words is 1",
