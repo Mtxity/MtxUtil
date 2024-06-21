cf1830d7df29d3f132ee80f6bde4d6b08c8252fc
Enrique <edavalos@ucsc.edu>
2024-06-21 10:07:22 -0700

    Added temp loaded data for MtxCsv

--- src/main/java/com/edavalos/mtx/util/generator/data/MtxCsv.java
+++ src/main/java/com/edavalos/mtx/util/generator/data/MtxCsv.java
@@ -1,12 +1,24 @@
 package com.edavalos.mtx.util.generator.data;
 
 public class MtxCsv {
+    private final String xChar = "x";
+    private final String yChar = "y";
+
     private final String[][] contents;
 
     public MtxCsv(int size) {
         this.contents = new String[size][];
         for (int i = 0; i < size; i++) {
             this.contents[i] = new String[size];
+        }
+        this.fillContentsWithGenericSamples();
+    }
+
+    private void fillContentsWithGenericSamples() {
+        for (int x = 0; x < this.contents.length; x++) {
+            for (int y = 0; y < this.contents[x].length; y++) {
+                this.contents[x][y] = this.xChar.repeat(x) + this.yChar.repeat(y);
+            }
         }
     }
 }
