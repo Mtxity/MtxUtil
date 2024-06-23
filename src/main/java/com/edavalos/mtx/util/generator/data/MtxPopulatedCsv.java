3dd37bfa8ebbef44bc13d5ea41e134c40af31f59
Enrique <edavalos@ucsc.edu>
2024-06-23 03:22:26 -0700

    Added loadSampleCsv() to MtxPopulatedCsv

--- src/main/java/com/edavalos/mtx/util/generator/data/MtxPopulatedCsv.java
+++ src/main/java/com/edavalos/mtx/util/generator/data/MtxPopulatedCsv.java
@@ -1,6 +1,15 @@
 package com.edavalos.mtx.util.generator.data;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MtxPopulatedCsv {
+    private static final String SAMPLE_CSV_FILEPATH = "./randomData.csv";
+    private static final String ERROR_SAMPLE_CSV_COULD_NOT_BE_LOADED = "Sample csv could not be loaded!";
+
     private final String xChar = "x";
     private final String yChar = "y";
 
@@ -27,6 +36,29 @@ public class MtxPopulatedCsv {
                 this.contents[x][y] = this.xChar.repeat(x) + this.yChar.repeat(y);
             }
         }
+    }
+
+    private static List<List<String>> loadSampleCsv() {
+        Scanner fileScanner;
+        try {
+            File sampleCsv = new File(MtxPopulatedCsv.class.getClassLoader().getResource(SAMPLE_CSV_FILEPATH).getFile());
+            fileScanner = new Scanner(sampleCsv);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(ERROR_SAMPLE_CSV_COULD_NOT_BE_LOADED);
+        }
+
+        List<List<String>> data = new ArrayList<>();
+        while (fileScanner.hasNextLine()) {
+            List<String> values = new ArrayList<>();
+            try (Scanner rowScanner = new Scanner(fileScanner.nextLine())) {
+                rowScanner.useDelimiter(",");
+                while (rowScanner.hasNext()) {
+                    values.add(rowScanner.next());
+                }
+            }
+            data.add(values);
+        }
+        return data;
     }
 
     public String get(int x, int y) {
