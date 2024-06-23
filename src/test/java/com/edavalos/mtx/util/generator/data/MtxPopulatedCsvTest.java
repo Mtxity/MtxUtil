24d5e6d5df4fe07ae0a426ac476ed706ea2f6bd9
Enrique <edavalos@ucsc.edu>
2024-06-23 04:12:51 -0700

    Completed code coverage for MtxPopulatedCsv

--- src/test/java/com/edavalos/mtx/util/generator/data/MtxPopulatedCsvTest.java
+++ src/test/java/com/edavalos/mtx/util/generator/data/MtxPopulatedCsvTest.java
@@ -3,7 +3,10 @@ package com.edavalos.mtx.util.generator.data;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class MtxPopulatedCsvTest {
     private MtxPopulatedCsv mtxPopulatedCsv;
@@ -38,4 +41,55 @@ public class MtxPopulatedCsvTest {
         }
     }
 
+    @Test
+    public void testGetAt() {
+        mtxPopulatedCsv = new MtxPopulatedCsv();
+
+        assertEquals("strawberry", mtxPopulatedCsv.getAt(0, 1));
+        assertEquals("kumquat", mtxPopulatedCsv.getAt(1, 2));
+        assertEquals("dill", mtxPopulatedCsv.getAt(2, 3));
+    }
+
+    @Test
+    public void testGetCopy() {
+        String[][] sampleContents = {{"a", "b"}, {"c", "d"}, {"e", "f"}};
+        mtxPopulatedCsv = new MtxPopulatedCsv(sampleContents);
+        assertArrayEquals(sampleContents, mtxPopulatedCsv.getCopy());
+
+        mtxPopulatedCsv.contents[0][1] = "g";
+        assertArrayEquals(sampleContents, mtxPopulatedCsv.getCopy());
+    }
+
+    @Test
+    public void testGetOriginal_editable() {
+        String[][] sampleContents = {{"a", "b"}, {"c", "d"}, {"e", "f"}};
+        mtxPopulatedCsv = new MtxPopulatedCsv(sampleContents);
+        try {
+            assertArrayEquals(sampleContents, mtxPopulatedCsv.getOriginal());
+        } catch (IllegalAccessException e) {
+            fail();
+        }
+
+        mtxPopulatedCsv.contents[0][1] = "g";
+        sampleContents[0][1] = "g";
+        try {
+            assertArrayEquals(sampleContents, mtxPopulatedCsv.getOriginal());
+        } catch (IllegalAccessException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetOriginal_nonEditable() {
+        String[][] sampleContents = {{"a", "b"}, {"c", "d"}, {"e", "f"}};
+        mtxPopulatedCsv = new MtxPopulatedCsv(sampleContents);
+        mtxPopulatedCsv.editable = false;
+
+        String expectedErrMsg = "This MtxPopulatedCsv cannot be modified! No direct access allowed!";
+        String actualErrMsg = assertThrows(
+                IllegalAccessException.class,
+                () -> mtxPopulatedCsv.getOriginal()
+        ).getMessage();
+        assertEquals(expectedErrMsg, actualErrMsg);
+    }
 }
