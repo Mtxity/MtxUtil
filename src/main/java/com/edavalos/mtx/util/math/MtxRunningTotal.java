6c1298c52c0d7164513775b0b886c488227a2589
Enrique <edavalos@ucsc.edu>
2024-07-11 20:23:34 -0700

    Added limit field to MtxRunningTotal

--- src/main/java/com/edavalos/mtx/util/math/MtxRunningTotal.java
+++ src/main/java/com/edavalos/mtx/util/math/MtxRunningTotal.java
@@ -4,13 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MtxRunningTotal {
+    private static final int DEFAULT_LIMIT = 100;
+
     private final List<Integer> integerList;
+    private final int limit;
 
     public MtxRunningTotal() {
         this.integerList = new ArrayList<>();
+        this.limit = DEFAULT_LIMIT;
     }
 
     public void add(int value) {
+        if (integerList.size() >= limit) {
+            integerList.remove(0);
+        }
+
         integerList.add(value);
     }
 
