6fa7a575fd96f10c4e824a61f1e22a70655ffbcd
Enrique <edavalos@ucsc.edu>
2024-07-11 19:22:04 -0700

    Added WIP MtxRunningTotal

---
+++ src/main/java/com/edavalos/mtx/util/math/MtxRunningTotal.java
@@ -0,0 +1,12 @@
+package com.edavalos.mtx.util.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MtxRunningTotal {
+    private final List<Integer> integerList;
+
+    public MtxRunningTotal() {
+        this.integerList = new ArrayList<>();
+    }
+}
