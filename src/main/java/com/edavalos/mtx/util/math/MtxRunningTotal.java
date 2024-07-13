f5d29737b4bbb747f444db2b5957cdd36b98ed1e
Enrique <edavalos@ucsc.edu>
2024-07-13 01:12:07 -0700

    Moved public methods shared logic into calculate() in MtxRunningTotal

--- src/main/java/com/edavalos/mtx/util/math/MtxRunningTotal.java
+++ src/main/java/com/edavalos/mtx/util/math/MtxRunningTotal.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MtxRunningTotal {
+    private enum Property {
+        AVERAGE, MEDIAN, MIN, MAX, SUM;
+    }
+
     private static final int DEFAULT_LIMIT = 100;
     private static final int MAX_LIMIT = 100000;
 
@@ -41,62 +45,70 @@ public class MtxRunningTotal {
     }
 
     public double getAvg(int lastNumbersToUse) {
-        if (lastNumbersToUse > this.integerList.size()) {
-            lastNumbersToUse = this.integerList.size();
-        }
-
-        int runningTotal = 0;
-        for (int i = this.integerList.size(); i >= this.integerList.size() - lastNumbersToUse; i--) {
-            runningTotal += this.integerList.get(i - 1);
-        }
-        return runningTotal / (double) lastNumbersToUse;
+        return this.calculate(Property.AVERAGE, lastNumbersToUse);
     }
 
     public double getMedian(int lastNumbersToUse) {
-        if (lastNumbersToUse > this.integerList.size()) {
-            lastNumbersToUse = this.integerList.size();
-        }
-
-        List<Integer> runningTotal = new ArrayList<>();
-        for (int i = this.integerList.size(); i >= this.integerList.size() - lastNumbersToUse; i--) {
-            runningTotal.add(this.integerList.get(i - 1));
-        }
-
-        int middleIdx = runningTotal.size() / 2;
-        if (lastNumbersToUse % 2 == 0) {
-            // If val is even, average the two middle values
-            return (runningTotal.get(middleIdx - 1) + runningTotal.get(middleIdx)) / 2.0;
-        } else {
-            // val is odd, return the middle value
-            return runningTotal.get(middleIdx);
-        }
+        return this.calculate(Property.MEDIAN, lastNumbersToUse);
     }
 
     public double getMax(int lastNumbersToUse) {
-        if (lastNumbersToUse > this.integerList.size()) {
-            lastNumbersToUse = this.integerList.size();
-        }
-
-        int max = 0;
-        for (int i = this.integerList.size(); i >= this.integerList.size() - lastNumbersToUse; i--) {
-            if (max < this.integerList.get(i - 1)) {
-                max = this.integerList.get(i - 1);
-            }
-        }
-        return max;
+        return this.calculate(Property.MAX, lastNumbersToUse);
     }
 
     public double getMin(int lastNumbersToUse) {
-        if (lastNumbersToUse > this.integerList.size()) {
-            lastNumbersToUse = this.integerList.size();
+        return this.calculate(Property.MIN, lastNumbersToUse);
+    }
+
+    private double calculate(Property property, int sampleSize) {
+        if (sampleSize > this.integerList.size()) {
+            sampleSize = this.integerList.size();
         }
 
-        int min = Integer.MAX_VALUE;
-        for (int i = this.integerList.size(); i >= this.integerList.size() - lastNumbersToUse; i--) {
-            if (min > this.integerList.get(i - 1)) {
-                min = this.integerList.get(i - 1);
+        return switch (property) {
+            case AVERAGE -> {
+                int runningTotal = 0;
+                for (int i = this.integerList.size(); i > this.integerList.size() - sampleSize; i--) {
+                    runningTotal += this.integerList.get(i - 1);
+                }
+                yield runningTotal / (double) sampleSize;
             }
-        }
-        return min;
+
+            case MEDIAN -> {
+                List<Integer> runningTotal = new ArrayList<>();
+                for (int i = this.integerList.size(); i > this.integerList.size() - sampleSize; i--) {
+                    runningTotal.add(this.integerList.get(i - 1));
+                }
+
+                int middleIdx = runningTotal.size() / 2;
+                if (sampleSize % 2 == 0) {
+                    // If val is even, average the two middle values
+                    yield (runningTotal.get(middleIdx - 1) + runningTotal.get(middleIdx)) / 2.0;
+                } else {
+                    // If val is odd, return the middle value
+                    yield runningTotal.get(middleIdx);
+                }
+            }
+
+            case MIN -> {
+                int min = Integer.MAX_VALUE;
+                for (int i = this.integerList.size(); i > this.integerList.size() - sampleSize; i--) {
+                    if (min > this.integerList.get(i - 1)) {
+                        min = this.integerList.get(i - 1);
+                    }
+                }
+                yield min;
+            }
+
+            case MAX -> {
+                int max = 0;
+                for (int i = this.integerList.size(); i > this.integerList.size() - sampleSize; i--) {
+                    if (max < this.integerList.get(i - 1)) {
+                        max = this.integerList.get(i - 1);
+                    }
+                }
+                yield max;
+            }
+        };
     }
 }
