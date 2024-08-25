c92634a5dfc916e8a9b511f2bfdefe3726c14573
Enrique <edavalos@ucsc.edu>
2024-08-25 02:44:24 -0700

    Added levenshteinDistance() method to MtxStringUtil

--- src/main/java/com/edavalos/mtx/util/string/MtxStringUtil.java
+++ src/main/java/com/edavalos/mtx/util/string/MtxStringUtil.java
@@ -453,4 +453,30 @@ public final class MtxStringUtil {
         }
         return new String[] {s};
     }
+
+    /**
+     * Calculates the levenshtein distance between two strings.
+     * Source: <a href="https://chatgpt.com/share/213d7e68-c706-4a81-9a85-0158758f91cf">ChatGPT</a>
+     * @param a first string to compare
+     * @param b second string to compare
+     * @return distance score between strings a and b
+     */
+    public static int levenshteinDistance(String a, String b) {
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+
+        for (int i = 0; i <= a.length(); i++) {
+            for (int j = 0; j <= b.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        return dp[a.length()][b.length()];
+    }
 }
