74f6bef1de1f3aa2fc9ff9ce9404db73cad63be1
Enrique <edavalos@ucsc.edu>
2024-09-02 03:30:47 -0700

    Added started field to MtxStopwatch

--- src/main/java/com/edavalos/mtx/util/math/MtxStopwatch.java
+++ src/main/java/com/edavalos/mtx/util/math/MtxStopwatch.java
@@ -2,20 +2,30 @@ package com.edavalos.mtx.util.math;
 
 public class MtxStopwatch {
     private long startTime;
+    private boolean started;
 
-    public MtxStopwatch() { }
+    public MtxStopwatch() {
+        this.startTime = 0;
+        this.started = false;
+    }
 
     public void start() {
         this.startTime = System.currentTimeMillis();
+        this.started = true;
     }
 
     public long lap() {
-        return System.currentTimeMillis() - startTime;
+        return System.currentTimeMillis() - this.startTime;
     }
 
     public long stop() {
-        long gap = System.currentTimeMillis() - startTime;
-        startTime = 0;
+        if (!this.started) {
+            return 0;
+        }
+
+        long gap = System.currentTimeMillis() - this.startTime;
+        this.startTime = 0;
+        this.started = false;
         return gap;
     }
 }
