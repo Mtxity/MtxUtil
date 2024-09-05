ec9dba9ead9ea63ebc6f39afac240366c937aa6d
Enrique <edavalos@ucsc.edu>
2024-09-05 01:19:21 -0700

    Added StartStopOnce tests to MtxStopwatchTest

--- src/test/java/com/edavalos/mtx/util/math/MtxStopwatchTest.java
+++ src/test/java/com/edavalos/mtx/util/math/MtxStopwatchTest.java
@@ -2,6 +2,12 @@ package com.edavalos.mtx.util.math;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MtxStopwatchTest {
 
@@ -14,7 +20,80 @@ public class MtxStopwatchTest {
 
     @Nested
     public class StartStopOnceTests {
-        // @TODO: Implement test for this
+
+        @Test
+        public void testStart() {
+            verifyStartingValues(mtxStopwatch);
+
+            mtxStopwatch.start();
+
+            assertNotEquals(0, mtxStopwatch.startTime);
+            assertTrue(mtxStopwatch.started);
+        }
+
+        @Test
+        public void testStop() {
+            verifyStartingValues(mtxStopwatch);
+
+            long gap = mtxStopwatch.stop();
+
+            assertEquals(mtxStopwatch.resetTime, 0);
+            assertFalse(mtxStopwatch.started);
+            assertEquals(0, gap);
+        }
+
+        @Test
+        public void testStartStop() throws InterruptedException {
+            verifyStartingValues(mtxStopwatch);
+
+            mtxStopwatch.start();
+            Thread.sleep(10);
+            long gap = mtxStopwatch.stop();
+
+            assertEquals(mtxStopwatch.resetTime, mtxStopwatch.startTime);
+            assertFalse(mtxStopwatch.started);
+            assertNotEquals(0, gap);
+        }
+
+        @Test
+        public void testGetLaps() throws InterruptedException {
+            verifyStartingValues(mtxStopwatch);
+
+            mtxStopwatch.start();
+            Thread.sleep(10);
+            mtxStopwatch.stop();
+
+            assertTrue(mtxStopwatch.getLaps().isEmpty());
+        }
+
+        @Test
+        public void testGetLastLap() throws InterruptedException {
+            verifyStartingValues(mtxStopwatch);
+
+            mtxStopwatch.start();
+            Thread.sleep(10);
+            mtxStopwatch.stop();
+
+            assertEquals(0, mtxStopwatch.getLastLap());
+        }
+
+        @Test
+        public void testGetResetTime() throws InterruptedException {
+            verifyStartingValues(mtxStopwatch);
+
+            mtxStopwatch.start();
+            Thread.sleep(10);
+            mtxStopwatch.stop();
+
+            assertNotEquals(0, mtxStopwatch.getResetTime());
+        }
+
+        private static void verifyStartingValues(MtxStopwatch sampleStopwatch) {
+            assertEquals(0, sampleStopwatch.startTime);
+            assertEquals(0, sampleStopwatch.resetTime);
+            assertFalse(sampleStopwatch.started);
+            assertTrue(sampleStopwatch.laps.isEmpty());
+        }
     }
 
     @Nested
