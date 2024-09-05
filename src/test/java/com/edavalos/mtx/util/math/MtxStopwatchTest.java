a10e40e999b052dfcfab4a1ee93645244900d1df
Enrique <edavalos@ucsc.edu>
2024-09-05 00:55:45 -0700

    Added Tests skeleton class for MtxStopwatch

---
+++ src/test/java/com/edavalos/mtx/util/math/MtxStopwatchTest.java
@@ -0,0 +1,49 @@
+package com.edavalos.mtx.util.math;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+
+public class MtxStopwatchTest {
+
+    private MtxStopwatch mtxStopwatch;
+
+    @BeforeEach
+    public void setUp() {
+        mtxStopwatch = new MtxStopwatch();
+    }
+
+    @Nested
+    public class StartStopOnceTests {
+        // @TODO: Implement test for this
+    }
+
+    @Nested
+    public class StartStopTwiceTests {
+        // @TODO: Implement test for this
+    }
+
+    @Nested
+    public class StartStopXTimesTests {
+        // @TODO: Implement test for this
+    }
+
+    @Nested
+    public class StartStopResetTests {
+        // @TODO: Implement test for this
+    }
+
+    @Nested
+    public class StartStopResetStartTests {
+        // @TODO: Implement test for this
+    }
+
+    @Nested
+    public class StartStopResetStartStopTests {
+        // @TODO: Implement test for this
+    }
+
+    @Nested
+    public class StartStopResetStartStopResetXTimesTests {
+        // @TODO: Implement test for this
+    }
+}
