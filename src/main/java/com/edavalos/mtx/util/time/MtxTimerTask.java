1e6605d42ffff50f720185b3b0d7502278d1d310
Enrique <edavalos@ucsc.edu>
2024-09-07 00:47:07 -0700

    Added MtxTimerTask

---
+++ src/main/java/com/edavalos/mtx/util/time/MtxTimerTask.java
@@ -0,0 +1,11 @@
+package com.edavalos.mtx.util.time;
+
+import java.util.TimerTask;
+
+public class MtxTimerTask extends TimerTask {
+
+    @Override
+    public void run() {
+        System.out.println("Test");
+    }
+}
