5c5b5e25f057e3171a786018693b14d4a427861d
Enrique <edavalos@ucsc.edu>
2024-09-07 01:08:19 -0700

    Added timesToRun field to MtxTimerTask

--- src/main/java/com/edavalos/mtx/util/time/MtxTimerTask.java
+++ src/main/java/com/edavalos/mtx/util/time/MtxTimerTask.java
@@ -6,10 +6,22 @@ import java.util.TimerTask;
 public class MtxTimerTask {
 
     private class MtxTimerTaskSchedule extends TimerTask {
+        private final int timesToRun;
+        private int timesRan;
+
+        public MtxTimerTaskSchedule(int timesToRun) {
+            this.timesToRun = timesToRun;
+            this.timesRan = 0;
+        }
 
         @Override
         public void run() {
+            if (this.timesRan > this.timesToRun) {
+                this.cancel();
+            }
+
             System.out.println("Test");
+            this.timesRan ++;
         }
     }
 
@@ -18,13 +30,14 @@ public class MtxTimerTask {
     private long period;
     private TimerTask timerTask;
 
-    public MtxTimerTask(long delay, long period) {
+    public MtxTimerTask(long delay, long period, int timesToRun) {
         this.timer = new Timer();
         this.delay = delay;
         this.period = period;
+        this.timerTask = new MtxTimerTaskSchedule(timesToRun);
     }
 
     public void start() {
-        timer.schedule(new MtxTimerTaskSchedule(), this.delay, this.period);
+        timer.schedule(this.timerTask, this.delay, this.period);
     }
 }
