719288bc2cb8eeb93132d458350180822c4cf28c
Enrique <edavalos@ucsc.edu>
2024-09-07 00:54:16 -0700

    Added MtxTimerTaskSchedule to MtxTimerTask
    Added constructor with required schedule params

--- src/main/java/com/edavalos/mtx/util/time/MtxTimerTask.java
+++ src/main/java/com/edavalos/mtx/util/time/MtxTimerTask.java
@@ -1,11 +1,23 @@
 package com.edavalos.mtx.util.time;
 
+import java.util.Timer;
 import java.util.TimerTask;
 
-public class MtxTimerTask extends TimerTask {
+public class MtxTimerTask {
 
-    @Override
-    public void run() {
-        System.out.println("Test");
+    private class MtxTimerTaskSchedule extends TimerTask {
+
+        @Override
+        public void run() {
+            System.out.println("Test");
+        }
+    }
+
+    private Timer timer;
+    private TimerTask timerTask;
+
+    public MtxTimerTask(long delay, long period) {
+        this.timer = new Timer();
+        this.timer.schedule(new MtxTimerTaskSchedule(), delay, period);
     }
 }
