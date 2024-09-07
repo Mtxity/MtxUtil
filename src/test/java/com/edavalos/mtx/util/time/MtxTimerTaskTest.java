9edef791efd4de415b73be4f1be1fbd3a5150561
Enrique <edavalos@ucsc.edu>
2024-09-07 02:11:08 -0700

    Saved sample executable method as a const in MtxTimerTaskTest

--- src/test/java/com/edavalos/mtx/util/time/MtxTimerTaskTest.java
+++ src/test/java/com/edavalos/mtx/util/time/MtxTimerTaskTest.java
@@ -7,12 +7,13 @@ public class MtxTimerTaskTest {
     private static final long SAMPLE_DELAY = 5;
     private static final long SAMPLE_PERIOD = 1;
     private static final int SAMPLE_TIMES_TO_RUN = 20;
+    private static final MtxTimerTask.Executable SAMPLE_METHOD = () -> System.out.println("Test");
 
     private MtxTimerTask mtxTimerTask;
 
     @BeforeEach
     public void setUp() {
-        mtxTimerTask = new MtxTimerTask(SAMPLE_DELAY, SAMPLE_PERIOD, SAMPLE_TIMES_TO_RUN);
+        mtxTimerTask = new MtxTimerTask(SAMPLE_METHOD, SAMPLE_DELAY, SAMPLE_PERIOD, SAMPLE_TIMES_TO_RUN);
     }
 
     @Test
