f19aa9e13b5ea2c0fcfeeb5e1879091ec6cc7c46
Enrique <edavalos@ucsc.edu>
2024-09-08 02:57:57 -0700

    Added frequency test in MtxTimerTaskTest

--- src/test/java/com/edavalos/mtx/util/time/MtxTimerTaskTest.java
+++ src/test/java/com/edavalos/mtx/util/time/MtxTimerTaskTest.java
@@ -3,23 +3,29 @@ package com.edavalos.mtx.util.time;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MtxTimerTaskTest {
     private static final long SAMPLE_DELAY = 1;
     private static final long SAMPLE_PERIOD = 1;
     private static final int SAMPLE_TIMES_TO_RUN = 1;
-    private static final MtxTimerTask.Executable SAMPLE_METHOD = () -> System.out.println("Test");
+
+    private static int SAMPLE_COUNTER;
+    private static final MtxTimerTask.Executable SAMPLE_METHOD = () -> SAMPLE_COUNTER ++;
 
     private MtxTimerTask mtxTimerTask;
 
     @BeforeEach
     public void setUp() {
+        SAMPLE_COUNTER = 0;
         mtxTimerTask = new MtxTimerTask(SAMPLE_METHOD, SAMPLE_DELAY, SAMPLE_PERIOD, SAMPLE_TIMES_TO_RUN);
     }
 
     @Test
-    public void testStart() {
+    public void testStart() throws InterruptedException {
         mtxTimerTask.start();
 
-        // @TODO: Update tests with actual expected method run times
+        Thread.sleep(10);
+        assertEquals(3, SAMPLE_COUNTER);
     }
 }
