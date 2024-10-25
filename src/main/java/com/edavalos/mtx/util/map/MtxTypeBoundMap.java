5e1c7f871afb606e8599ffd62bb57f20d7723c3e
Enrique <edavalos@ucsc.edu>
2024-10-25 01:48:29 -0700

    Added MtxTypeBoundMap

---
+++ src/main/java/com/edavalos/mtx/util/map/MtxTypeBoundMap.java
@@ -0,0 +1,79 @@
+package com.edavalos.mtx.util.map;
+
+import java.lang.invoke.WrongMethodTypeException;
+import java.util.HashMap;
+
+public class MtxTypeBoundMap extends HashMap<String, Object> {
+
+    public String getString(String key) {
+        if (super.get(key) == null) {
+            return null;
+        }
+
+        try {
+            return (String) super.get(key);
+        } catch (ClassCastException cce) {
+            throw new WrongMethodTypeException("Value for key '" + key + "' is not a string");
+        }
+    }
+
+    public Integer getInteger(String key) {
+        if (super.get(key) == null) {
+            return null;
+        }
+
+        try {
+            return (Integer) super.get(key);
+        } catch (ClassCastException cce) {
+            throw new WrongMethodTypeException("Value for key '" + key + "' is not an integer");
+        }
+    }
+
+    public Boolean getBoolean(String key) {
+        if (super.get(key) == null) {
+            return null;
+        }
+
+        try {
+            return (Boolean) super.get(key);
+        } catch (ClassCastException cce) {
+            throw new WrongMethodTypeException("Value for key '" + key + "' is not a boolean");
+        }
+    }
+
+    public Double getDouble(String key) {
+        if (super.get(key) == null) {
+            return null;
+        }
+
+        try {
+            return (Double) super.get(key);
+        } catch (ClassCastException cce) {
+            throw new WrongMethodTypeException("Value for key '" + key + "' is not a double");
+        }
+    }
+
+    public Long getLong(String key) {
+        if (super.get(key) == null) {
+            return null;
+        }
+
+        try {
+            return (Long) super.get(key);
+        } catch (ClassCastException cce) {
+            throw new WrongMethodTypeException("Value for key '" + key + "' is not a long");
+        }
+    }
+
+    public Character getCharacter(String key) {
+        if (super.get(key) == null) {
+            return null;
+        }
+
+        try {
+            return (Character) super.get(key);
+        } catch (ClassCastException cce) {
+            throw new WrongMethodTypeException("Value for key '" + key + "' is not a character");
+        }
+    }
+}
