537512e52cf2ac71240d887ffa63391c14112988
Enrique <edavalos@ucsc.edu>
2025-11-25 01:44:21 -0800

    Added unit tests for isPalindrome() in MtxLinkedList

--- src/test/java/com/edavalos/mtx/util/list/MtxLinkedListTest.java
+++ src/test/java/com/edavalos/mtx/util/list/MtxLinkedListTest.java
@@ -1,5 +1,6 @@
 package com.edavalos.mtx.util.list;
 
+import com.edavalos.mtx.util.list.line.MtxStack;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -620,6 +622,65 @@ public final class MtxLinkedListTest {
             integerMtxLinkedList.reverse();
 
             assertArrayEquals(TEST_ARRAY, integerMtxLinkedList.toArray());
+        }
+    }
+
+    @Nested
+    class IsPalindromeTests {
+        private static final Integer[] YES_PALINDROME = {30, 60, 90, 60, 30};
+        private static final Integer[] NOT_PALINDROME = {30, 60, 90, 120, 150};
+        private static final Integer[] EMPTY = {};
+        private static final int LARGE_TEST_SIZE = 300;
+
+        @Test
+        public void testIsPalindrome_yes_short() {
+            MtxLinkedList<Integer> integerMtxLinkedList = new MtxLinkedList<>(YES_PALINDROME);
+            assertTrue(integerMtxLinkedList.isPalindrome());
+        }
+
+        @Test
+        public void testIsPalindrome_yes_long() {
+            Random random = new Random();
+            MtxStack<Integer> values = new MtxStack<>();
+            Integer[] testArray = new Integer[LARGE_TEST_SIZE + 1];
+            for (int i = 0; i <= LARGE_TEST_SIZE / 2; i++) {
+                int randomInt = random.nextInt(1, 100);
+                values.push(randomInt);
+                testArray[i] = randomInt;
+            }
+            for (int i = LARGE_TEST_SIZE / 2; i <= LARGE_TEST_SIZE; i++) {
+                testArray[i] = values.pop();
+            }
+
+            MtxLinkedList<Integer> integerMtxLinkedList = new MtxLinkedList<>(testArray);
+            assertTrue(integerMtxLinkedList.isPalindrome());
+        }
+
+        @Test
+        public void testIsPalindrome_yes_empty() {
+            MtxLinkedList<Integer> integerMtxLinkedList = new MtxLinkedList<>(EMPTY);
+            assertTrue(integerMtxLinkedList.isPalindrome());
+        }
+
+        @Test
+        public void testIsPalindrome_no_short() {
+            MtxLinkedList<Integer> integerMtxLinkedList = new MtxLinkedList<>(NOT_PALINDROME);
+            assertFalse(integerMtxLinkedList.isPalindrome());
+        }
+
+        @Test
+        public void testIsPalindrome_no_long() {
+            Random random = new Random();
+            MtxStack<Integer> values = new MtxStack<>();
+            Integer[] testArray = new Integer[LARGE_TEST_SIZE];
+            for (int i = 0; i < LARGE_TEST_SIZE; i++) {
+                int randomInt = random.nextInt(1, 100);
+                values.push(randomInt);
+                testArray[i] = randomInt;
+            }
+
+            MtxLinkedList<Integer> integerMtxLinkedList = new MtxLinkedList<>(testArray);
+            assertFalse(integerMtxLinkedList.isPalindrome());
         }
     }
 }
