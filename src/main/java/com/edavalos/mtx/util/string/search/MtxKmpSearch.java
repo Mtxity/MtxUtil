563b2213779bb58a1e5b37d86d14f15831c7e85c
Enrique <edavalos@ucsc.edu>
2025-11-28 01:45:31 -0800

    Added MtxKmpSearch

---
+++ src/main/java/com/edavalos/mtx/util/string/search/MtxKmpSearch.java
@@ -0,0 +1,55 @@
+package com.edavalos.mtx.util.string.search;
+
+// Knuth–Morris–Pratt
+public class MtxKmpSearch {
+    private final String text;
+
+    public MtxKmpSearch(String text) {
+        this.text = text;
+    }
+
+    public int search(String pattern) {
+        int n = this.text.length();
+        int m = pattern.length();
+
+        int[] lps = buildLPS(pattern);
+        int i = 0, j = 0;
+
+        while (i < n) {
+            if (this.text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+                if (j == m) return i - j;  // match found
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static int[] buildLPS(String pattern) {
+        int m = pattern.length();
+        int[] lps = new int[m];
+        int len = 0;
+        int i = 1;
+
+        while (i < m) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                lps[i++] = ++len;
+            } else if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
+            }
+        }
+        return lps;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+}
