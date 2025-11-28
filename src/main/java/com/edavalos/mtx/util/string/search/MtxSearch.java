23fb575a0e2d26ae5a2511caa3bde150dd86307a
Enrique <edavalos@ucsc.edu>
2025-11-28 02:48:07 -0800

    Added contains() to MtxSearch

--- src/main/java/com/edavalos/mtx/util/string/search/MtxSearch.java
+++ src/main/java/com/edavalos/mtx/util/string/search/MtxSearch.java
@@ -4,5 +4,9 @@ public interface MtxSearch {
 
     int search(String pattern);
 
+    default boolean contains(String pattern) {
+        return this.search(pattern) != -1;
+    }
+
     String getText();
 }
