833b7305bd32773aea3de5330d783b72438766e7
Enrique <edavalos@ucsc.edu>
2025-11-28 02:31:45 -0800

    Added unit tests for search() in MtxSearch implementations

--- src/test/java/com/edavalos/mtx/util/string/search/MtxSearchTest.java
+++ src/test/java/com/edavalos/mtx/util/string/search/MtxSearchTest.java
@@ -1,6 +1,7 @@
 package com.edavalos.mtx.util.string.search;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,5 +28,54 @@ public class MtxSearchTest {
         assertEquals(SAMPLE_TEXT, mtxBoyerMooreSearch.getText());
         assertEquals(SAMPLE_TEXT, mtxKnuthMorrisPrattSearch.getText());
         assertEquals(SAMPLE_TEXT, mtxRabinKarpSearch.getText());
+    }
+
+    @Nested
+    class SearchTests {
+        private static final String SAMPLE_PATTERN_MIDDLE = "sample";
+        private static final String SAMPLE_PATTERN_BEGINNING = "This";
+        private static final String SAMPLE_PATTERN_END = "in.";
+        private static final String SAMPLE_PATTERN_NO_MATCH = "xyz";
+        private static final String SAMPLE_PATTERN_EMPTY = "";
+
+        @Test
+        public void testSearch_middle() {
+            assertEquals(10, mtxNativeSearch.search(SAMPLE_PATTERN_MIDDLE));
+            assertEquals(10, mtxBoyerMooreSearch.search(SAMPLE_PATTERN_MIDDLE));
+            assertEquals(10, mtxKnuthMorrisPrattSearch.search(SAMPLE_PATTERN_MIDDLE));
+            assertEquals(10, mtxRabinKarpSearch.search(SAMPLE_PATTERN_MIDDLE));
+        }
+
+        @Test
+        public void testSearch_beginning() {
+            assertEquals(0, mtxNativeSearch.search(SAMPLE_PATTERN_BEGINNING));
+            assertEquals(0, mtxBoyerMooreSearch.search(SAMPLE_PATTERN_BEGINNING));
+            assertEquals(0, mtxKnuthMorrisPrattSearch.search(SAMPLE_PATTERN_BEGINNING));
+            assertEquals(0, mtxRabinKarpSearch.search(SAMPLE_PATTERN_BEGINNING));
+        }
+
+        @Test
+        public void testSearch_end() {
+            assertEquals(32, mtxNativeSearch.search(SAMPLE_PATTERN_END));
+            assertEquals(32, mtxBoyerMooreSearch.search(SAMPLE_PATTERN_END));
+            assertEquals(32, mtxKnuthMorrisPrattSearch.search(SAMPLE_PATTERN_END));
+            assertEquals(32, mtxRabinKarpSearch.search(SAMPLE_PATTERN_END));
+        }
+
+        @Test
+        public void testSearch_noMatch() {
+            assertEquals(-1, mtxNativeSearch.search(SAMPLE_PATTERN_NO_MATCH));
+            assertEquals(-1, mtxBoyerMooreSearch.search(SAMPLE_PATTERN_NO_MATCH));
+            assertEquals(-1, mtxKnuthMorrisPrattSearch.search(SAMPLE_PATTERN_NO_MATCH));
+            assertEquals(-1, mtxRabinKarpSearch.search(SAMPLE_PATTERN_NO_MATCH));
+        }
+
+        @Test
+        public void testSearch_emptyPattern() {
+            assertEquals(-1, mtxNativeSearch.search(SAMPLE_PATTERN_EMPTY));
+            assertEquals(-1, mtxBoyerMooreSearch.search(SAMPLE_PATTERN_EMPTY));
+            assertEquals(-1, mtxKnuthMorrisPrattSearch.search(SAMPLE_PATTERN_EMPTY));
+            assertEquals(-1, mtxRabinKarpSearch.search(SAMPLE_PATTERN_EMPTY));
+        }
     }
 }
