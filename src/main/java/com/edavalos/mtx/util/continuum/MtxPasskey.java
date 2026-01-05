41cef2de804eda5507bb775a0655197cb1aade8c
Enrique <edavalos@ucsc.edu>
2026-01-04 23:18:04 -0800

    Added constructor and documentation to MtxPasskey

--- src/main/java/com/edavalos/mtx/util/continuum/MtxPasskey.java
+++ src/main/java/com/edavalos/mtx/util/continuum/MtxPasskey.java
@@ -2,7 +2,27 @@ package com.edavalos.mtx.util.continuum;
 
 import java.util.Iterator;
 
+/**
+ * MtxPasskey is a passkey holder that auto-increments passkeys that require periodic rotating.
+ * initialPasskey is the value first used for passkey1, and passkeyIncrement is used to add to
+ * the passkey every passkeyLength interval.
+ */
 public class MtxPasskey implements Iterable<String> {
+    private final String initialPasskey;
+    private final int passkeyIncrement;
+    private final int passkeyLength;
+
+    /**
+     * Create new MtxPasskey
+     * @param initialPasskey String to use as base / first passkey
+     * @param passkeyIncrement Integer to add to passkey every passkeyLength (as a String)
+     * @param passkeyLength Number of times to increment passkey before rotating entire result
+     */
+    public MtxPasskey(String initialPasskey, int passkeyIncrement, int passkeyLength) {
+        this.initialPasskey = initialPasskey;
+        this.passkeyIncrement = passkeyIncrement;
+        this.passkeyLength = passkeyLength;
+    }
 
     /**
      * Returns an iterator over elements of type {@code T}.
