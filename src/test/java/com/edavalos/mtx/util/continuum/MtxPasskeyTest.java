2b77a586003f6aa8b4c4f1e09a4dfaabd866b37d
Enrique <edavalos@ucsc.edu>
2026-01-04 23:33:27 -0800

    Added unit tests for getNextPasskey() in MtxPasskey

---
+++ src/test/java/com/edavalos/mtx/util/continuum/MtxPasskeyTest.java
@@ -0,0 +1,31 @@
+package com.edavalos.mtx.util.continuum;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public final class MtxPasskeyTest {
+    private MtxPasskey mtxPasskey;
+
+    @Nested
+    class GetNextPasskeyTests {
+
+        @Test
+        public void testGetNextPasskey_incrementsCorrectly() {
+            mtxPasskey = new MtxPasskey("key", 10, 5);
+
+            assertEquals("key0", mtxPasskey.getNextPasskey());
+            assertEquals("key1", mtxPasskey.getNextPasskey());
+            assertEquals("key2", mtxPasskey.getNextPasskey());
+        }
+
+        @Test
+        public void testGetNextPasskey_handlesEmptyInitialPasskey() {
+            mtxPasskey = new MtxPasskey("", 1, 1);
+
+            assertEquals("0", mtxPasskey.getNextPasskey());
+            assertEquals("1", mtxPasskey.getNextPasskey());
+        }
+    }
+}
