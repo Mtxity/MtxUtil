ddbc540514ea7a18aecc7a8767a91d112dc843b3
Enrique <edavalos@ucsc.edu>
2026-01-04 23:12:55 -0800

    Added WIP MtxPasskey

---
+++ src/main/java/com/edavalos/mtx/util/continuum/MtxPasskey.java
@@ -0,0 +1,16 @@
+package com.edavalos.mtx.util.continuum;
+
+import java.util.Iterator;
+
+public class MtxPasskey implements Iterable<String> {
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<String> iterator() {
+        return null;
+    }
+}
