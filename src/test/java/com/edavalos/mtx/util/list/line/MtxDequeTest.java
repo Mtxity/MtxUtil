26fb3dd366a39f64e85f47219a0a9c2d594631c1
Enrique <edavalos@ucsc.edu>
2026-03-07 17:16:47 -0800

    Added unit test for dequeueFront()

--- src/test/java/com/edavalos/mtx/util/list/line/MtxDequeTest.java
+++ src/test/java/com/edavalos/mtx/util/list/line/MtxDequeTest.java
@@ -1,9 +1,12 @@
 package com.edavalos.mtx.util.list.line;
 
+import com.edavalos.mtx.util.list.MtxArrayList;
+import com.edavalos.mtx.util.list.MtxList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public final class MtxDequeTest {
     private MtxDeque<Integer> mtxDeque;
@@ -33,5 +36,22 @@ public final class MtxDequeTest {
         }
 
         assertEquals(elementsListString, mtxDeque.toString());
+    }
+
+    @Test
+    public void testDequeueFront() {
+        int elements = 5;
+        String elementsListString = "[0, 1, 2, 3, 4]";
+        for (int i = 0; i < elements; i++) {
+            mtxDeque.enqueueFront(i);
+        }
+
+        MtxList<Integer> dequeues = new MtxArrayList<>();
+        for (int j = 0; j < elements; j++) {
+            dequeues.add(mtxDeque.dequeueFront());
+        }
+
+        assertEquals(elementsListString, dequeues.toString());
+        assertNull(mtxDeque.dequeueFront());
     }
 }
