d9319d103d925c79bf36575fffcdc5832004ca45
Enrique <edavalos@ucsc.edu>
2026-05-16 01:26:42 -0700

    Added pickRandom() for six values in MtxRandomSelector

--- src/main/java/com/edavalos/mtx/util/math/collection/MtxRandomSelector.java
+++ src/main/java/com/edavalos/mtx/util/math/collection/MtxRandomSelector.java
@@ -55,4 +55,21 @@ public final class MtxRandomSelector {
         int index = ThreadLocalRandom.current().nextInt(2);
         return index == 1 ? MtxOptionalVar.ofNullable(item2) : MtxOptionalVar.ofNullable(item1);
     }
+
+    public static <T> MtxOptionalVar<T> pickRandom(T item1, T item2, T item3, T item4, T item5, T item6) {
+        if (item1 == null || item2 == null || item3 == null || item4 == null || item5 == null || item6 == null) {
+            return MtxOptionalVar.empty();
+        }
+
+        int index = ThreadLocalRandom.current().nextInt(6);
+        return switch (index) {
+            case 0 -> MtxOptionalVar.ofNullable(item1);
+            case 1 -> MtxOptionalVar.ofNullable(item2);
+            case 2 -> MtxOptionalVar.ofNullable(item3);
+            case 3 -> MtxOptionalVar.ofNullable(item4);
+            case 4 -> MtxOptionalVar.ofNullable(item5);
+            case 5 -> MtxOptionalVar.ofNullable(item6);
+            default -> MtxOptionalVar.empty();
+        };
+    }
 }
