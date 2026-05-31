465f9e6262f3b30009bb899bd0cf7e671e563c8d
Enrique <edavalos@ucsc.edu>
2026-05-30 23:57:24 -0700

    Added pickRandom() for seven values in MtxRandomSelector

--- src/main/java/com/edavalos/mtx/util/math/collection/MtxRandomSelector.java
+++ src/main/java/com/edavalos/mtx/util/math/collection/MtxRandomSelector.java
@@ -130,6 +130,27 @@ public final class MtxRandomSelector {
         };
     }
 
+    public static <T> MtxOptionalVar<T> pickRandom(T item1, T item2, T item3, T item4, T item5, T item6, T item7) {
+        if (
+                item1 == null && item2 == null && item3 == null && item4 == null
+             && item5 == null && item6 == null && item7 == null
+        ) {
+            return MtxOptionalVar.empty();
+        }
+
+        int index = nextRandomInt(7);
+        return switch (index) {
+            case 0 -> MtxOptionalVar.ofNullable(item1);
+            case 1 -> MtxOptionalVar.ofNullable(item2);
+            case 2 -> MtxOptionalVar.ofNullable(item3);
+            case 3 -> MtxOptionalVar.ofNullable(item4);
+            case 4 -> MtxOptionalVar.ofNullable(item5);
+            case 5 -> MtxOptionalVar.ofNullable(item6);
+            case 6 -> MtxOptionalVar.ofNullable(item7);
+            default -> MtxOptionalVar.empty();
+        };
+    }
+
     public static <T> MtxOptionalVar<T> pickRandom(T item1, T item2, T item3, T item4, T item5, T item6, T item7, T item8) {
         if (
                 item1 == null && item2 == null && item3 == null && item4 == null
