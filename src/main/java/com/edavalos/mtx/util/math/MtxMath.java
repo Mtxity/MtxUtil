e23350d45baf55f0e13ee323e08dd09ae9eb0a63
Enrique <edavalos@ucsc.edu>
2026-06-01 00:26:56 -0700

    Added dividesEvenlyEitherWay() to MtxMath

--- src/main/java/com/edavalos/mtx/util/math/MtxMath.java
+++ src/main/java/com/edavalos/mtx/util/math/MtxMath.java
@@ -393,4 +393,12 @@ public final class MtxMath {
     public static boolean hasRemainder(int xDiv, int yDev) {
         return xDiv % yDev != 0;
     }
+
+    public static boolean dividesEvenlyEitherWay(int a, int b) {
+        if (a == 0 || b == 0) {
+            return false;
+        }
+
+        return a % b == 0 || b % a == 0;
+    }
 }
