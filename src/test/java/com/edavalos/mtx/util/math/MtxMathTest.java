18f144830fb683750f4d18c816ae1b9a82ac39c9
Enrique <edavalos@ucsc.edu>
2026-06-01 00:19:13 -0700

    Fixed unit tests for hasRemainder()

--- src/test/java/com/edavalos/mtx/util/math/MtxMathTest.java
+++ src/test/java/com/edavalos/mtx/util/math/MtxMathTest.java
@@ -534,11 +534,11 @@ public class MtxMathTest {
 
         @Test
         public void testHasRemainder_true() {
-            assertTrue(MtxMath.hasRemainder(6, 3));
+            assertTrue(MtxMath.hasRemainder(7, 3));
         }
         @Test
         public void testHasRemainder_false() {
-            assertTrue(MtxMath.hasRemainder(7, 3));
+            assertFalse(MtxMath.hasRemainder(6, 3));
         }
     }
 }
