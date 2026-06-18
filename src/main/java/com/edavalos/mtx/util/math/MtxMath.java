f2810e7d65e1c1688a4c1b5a5dbfa97cb0b635be
Enrique <edavalos@ucsc.edu>
2026-06-17 23:59:19 -0700

    Added javadoc for Ln(2) constant in MtxMath

--- src/main/java/com/edavalos/mtx/util/math/MtxMath.java
+++ src/main/java/com/edavalos/mtx/util/math/MtxMath.java
@@ -27,6 +27,9 @@ public final class MtxMath {
      * The golden ratio, or Phi (𝜑) to the 32nd decimal place
      */
     public static final double PHI = 1.61803398874989484820458683436564;
+    /**
+     * The natural logarithm of 2
+     */
     public static final double LN2 = 0.69314718055994530941723212145818;
 
     public static class MtxFactorial implements Iterable<Integer> {
