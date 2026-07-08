a3a865bd6c080dfc44f44816c49199086f152341
Enrique <edavalos@ucsc.edu>
2026-07-07 21:09:08 -0700

    Implemented distanceBetweenPoints() in MtxMath

--- src/main/java/com/edavalos/mtx/util/math/MtxMath.java
+++ src/main/java/com/edavalos/mtx/util/math/MtxMath.java
@@ -786,4 +786,10 @@ public final class MtxMath {
     public static long round(double x) {
         return (long) floor(x + 0.5);
     }
+
+    public static double distanceBetweenPoints(double x1, double y1, double x2, double y2) {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+        return hypotenuse(dx, dy);
+    }
 }
