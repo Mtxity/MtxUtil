6bb96f9e9e73a3b394a4e8e2f6dedf6c23001dbb
Enrique <edavalos@ucsc.edu>
2026-07-11 00:32:44 -0700

    Added MtxComplex

---
+++ src/main/java/com/edavalos/mtx/util/math/MtxComplex.java
@@ -0,0 +1,11 @@
+package com.edavalos.mtx.util.math;
+
+public class MtxComplex {
+    private final double re; // Real part
+    private final double im; // Imaginary part
+
+    public MtxComplex(double real, double imag) {
+        this.re = real;
+        this.im = imag;
+    }
+}
